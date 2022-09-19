73a5f85c62347c0b2a3fab4ce6764f96b4283cd9
Tin Buenafe <78692266+tinbuenafe@users.noreply.github.com>
2022-09-19 11:06:58 +0800

    Working on data

--- test.R
+++ test.R
@@ -10,3 +10,36 @@ angaus.tc5.lr01$fitted.vars # variance of fitted valu on the response scale
 angaus.tc5.lr01$residuals # residuals for the fitted values
 angaus.tc5.lr01$contributions # relative importance of the variables
 angaus.tc5.lr01$cv.statistics
+
+#### Grid search ####
+
+library(tidyverse)
+
+d <- mtcars %>% 
+  # Convert `am` to factor and select relevant variables
+  mutate(am = factor(am, labels = c("Automatic", "Manual"))) %>% 
+  select(am, mpg, hp)
+
+ggplot(d, aes(mpg, hp, color = am)) +
+  geom_point()
+
+library(rpart)
+library(rpart.plot)
+
+# Set minsplit = 2 to fit every data point
+full_fit <- rpart(am ~ mpg + hp, data = d, minsplit = 2)
+prp(full_fit)
+
+# Training-test split
+set.seed(245)
+n <- nrow(d)
+train_rows <- sample(seq(n), size = .8 * n)
+train <- d[ train_rows, ]
+test  <- d[-train_rows, ]
+
+# Create the grid
+# Define a named list of parameter values
+gs <- list(minsplit = c(2, 5, 10),
+           maxdepth = c(1, 3, 8)) %>% 
+  cross_df() # Convert to data frame grid
+gs
