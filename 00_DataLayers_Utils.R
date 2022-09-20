d4d505d207da60fe37becf1e715cb9c48c07475b
Tin Buenafe <78692266+tinbuenafe@users.noreply.github.com>
2022-09-20 10:27:44 +0800

    Intersecting data

--- 00_DataLayers_Utils.R
+++ 00_DataLayers_Utils.R
@@ -8,32 +8,43 @@ suppressPackageStartupMessages({
   library(patchwork)
   library(magrittr)
   library(FNN)
+  library(raster)
+  library(VoCC)
 })
+
+lonlat <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 
 # Load worldwide landmass
 landmass <- rnaturalearth::ne_countries() %>% 
-  sf::st_as_sf(crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+  sf::st_as_sf(crs = lonlat)
 
-# Convert netcdf data (across years) to the model's 30-year mean
+# Convert netcdf to sf objects
 nc2sf <- function(model, expt, var) {
-  path <- "Data/Climatology/ESM/"
+  path <- "Data/Climatology/final1/"
   list <- list.files(path)
   param <- c(model, expt, var)
   
   file <- apply(outer(list, param, stringr::str_detect), 1, all) %>% as.numeric()
   file <- which(file == 1)
   
-  rs <- terra::rast(paste0(path, list[file])) %>%  # Create RasterStack
-    terra::as.polygons(trunc = FALSE, dissolve = FALSE, na.rm=FALSE) %>%
-    sf::st_as_sf(crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+  rs <- terra::rast(paste0(path, list[file])) # Create RasterStack
   
-  sf_use_s2(FALSE)
-  x <- st_intersects(rs, landmass, sparse = FALSE) %>% 
-    apply(., 1, any) # returns indices of rows in the data that have intersected with the landmass
-  rs[which(x==TRUE), 1:30] <- NA # Change those that intersect with land with NAs.
+  names(rs) <- paste("X", seq(from = 2015, to = 2100, by = 1), sep = "")
   
-  fin <- rs %>% 
-    dplyr::mutate(mean = rowMeans(across(1:30), na.rm = FALSE)) %>% 
+  sf <- rs %>%  
+    terra::subset(., paste("X", seq(from = 2022, to = 2050, by = 1), sep = "")) %>% # 2022-2050
+    terra::as.polygons(trunc = FALSE, dissolve = FALSE, na.rm=FALSE) %>% # Convert to sf polygon
+    sf::st_as_sf(crs = lonlat)
+  
+ # Removing degree grids that wholly or partially intersect with the landmass object.
+  logi_Reg <- sf::st_centroid(sf) %>%
+    sf::st_intersects(landmass) %>%
+    lengths > 0 # Get logical vector instead of sparse geometry binary
+  
+  int <- sf[!logi_Reg, ]
+  
+  fin <- int %>% 
+    dplyr::mutate(mean = rowMeans(across(1:(ncol(int)-1)), na.rm = FALSE)) %>% 
     dplyr::select(mean)
   
   return(fin)
--- 01a_DataLayers_Fish.R
+++ 01a_DataLayers_Fish.R
@@ -3,26 +3,50 @@ source("00_DataLayers_Utils.R")
 inPath <- "Data/"
 
 # assemble data
-YFT1 <- vroom::vroom(file = paste0(inPath, "Fish/", "CSVFile_", "yellowfin-tuna", "_jan-mar.csv"),
-                     show_col_types = FALSE) %>% 
-  dplyr::select(-FAO_Major_Fishing_Areas, -FAO_CWP_Code)
-YFT2 <- vroom::vroom(file = paste0(inPath, "Fish/", "CSVFile_", "yellowfin-tuna", "_apr-jun.csv"),
-                     show_col_types = FALSE) %>% 
-  dplyr::select(-FAO_Major_Fishing_Areas, -FAO_CWP_Code)
-YFT3 <- vroom::vroom(file = paste0(inPath, "Fish/", "CSVFile_", "yellowfin-tuna", "_jul-sept.csv"),
-                     show_col_types = FALSE) %>% 
-  dplyr::select(-FAO_Major_Fishing_Areas, -FAO_CWP_Code)
-YFT4 <- vroom::vroom(file = paste0(inPath, "Fish/", "CSVFile_", "yellowfin-tuna", "_oct-dec.csv"),
-                     show_col_types = FALSE) %>% 
-  dplyr::select(-FAO_Major_Fishing_Areas, -FAO_CWP_Code)
+YFTsf1 <- readRDS(paste0(inPath, "Fish/", "VectorFile_", "yellowfin-tuna", "_jan-mar.rds")) %>% 
+  tibble::as_tibble()
+YFTsf2 <- readRDS(paste0(inPath, "Fish/", "VectorFile_", "yellowfin-tuna", "_apr-jun.rds")) %>% 
+  tibble::as_tibble()
+YFTsf3 <- readRDS(paste0(inPath, "Fish/", "VectorFile_", "yellowfin-tuna", "_jul-sept.rds")) %>% 
+  tibble::as_tibble()
+YFTsf4 <- readRDS(paste0(inPath, "Fish/", "VectorFile_", "yellowfin-tuna", "_oct-dec.rds")) %>% 
+  tibble::as_tibble()
 
 # join all data.frames
-YFT <- dplyr::bind_rows(YFT1, YFT2, YFT3, YFT4) %>% 
+YFT <- dplyr::bind_rows(YFTsf1, YFTsf2, YFTsf3, YFTsf4) %>% 
   dplyr::mutate(cellID = factor(row_number())) %>% 
   dplyr::mutate(across(where(is.character), ~factor(.)),
                 abundance = ifelse(abundance > 0, yes = 1, no = 0)) %>% 
-  dplyr::select(cellID, species, abundance, everything()) %>% 
-  as.data.frame()
+  dplyr::select(cellID, species, abundance, everything()) #%>% 
+#  as.data.frame()
+YFTsf <- YFT %>% 
+  sf::st_as_sf(sf_column_name = "geometry") %>% 
+  sf::st_centroid()
+
+#### Calling climatology data ####
+tos <- nc2sf(model = "ACCESS-ESM1-5",
+             expt = "ssp585",
+             var = "tos") %>% 
+  dplyr::mutate(cellID = row_number()) %>% 
+  dplyr::rename(tos = mean)
+o2os <- nc2sf(model = "ACCESS-ESM1-5",
+              expt = "ssp585",
+              var = "o2os") %>% 
+  dplyr::mutate(cellID = row_number()) %>% 
+  dplyr::rename(o2os = mean)
+phos <- nc2sf(model = "CMCC-ESM2",
+              expt = "ssp585",
+              var = "phos") %>% 
+  dplyr::mutate(cellID = row_number()) %>% 
+  dplyr::rename(phos = mean)
+
+tos_df <- tibble::as_tibble(tos)
+YFT_df <- tibble::as_tibble(YFTsf)
+
+join <- dplyr::left_join(tos_df, YFT_df, by = "geometry")
+
+join <- sf::st_join(tos, YFTsf, join = st_contains_properly, left = TRUE) %>% 
+  dplyr::filter(!is.na(abundance))
 
 # dummy
 YFT.model0 <- dismo::gbm.step(data = YFT, gbm.x = 4:6,
