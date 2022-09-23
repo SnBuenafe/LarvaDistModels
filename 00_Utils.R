204b90f02a53593e5930e70d9dd48153601e74b3
Tin <tinbuenafe@gmail.com>
2022-09-23 18:12:44 +0800

    Update 00_Utils.R

--- 00_Utils.R
+++ 00_Utils.R
@@ -33,7 +33,11 @@ nc2sf <- function(model, expt, var) {
   sf <- rs %>%  
     terra::subset(., paste("X", seq(from = 2022, to = 2050, by = 1), sep = "")) %>% # 2022-2050
     terra::as.polygons(trunc = FALSE, dissolve = FALSE, na.rm=FALSE) %>% # Convert to sf polygon
-    sf::st_as_sf(crs = lonlat)
+    sf::st_as_sf() %>% 
+    sf::st_make_valid()
+  
+  sf::st_crs(sf) <- lonlat # set the CRS
+  sf %<>% sf::st_transform(crs = moll) # then transform
   
  # Removing degree grids that wholly or partially intersect with the landmass object.
   logi_Reg <- sf::st_centroid(sf) %>%
@@ -60,37 +64,74 @@ combineFish <- function(species) {
   
   df <- list()
   for(i in 1:length(file)) {
-    sf <- readRDS(file.path(path, list[file[i]])) %>% 
+    df[[i]] <- readRDS(file.path(path, list[file[i]])) %>% 
       tibble::as_tibble()
   }
   combined <- do.call(bind_rows, df) %>% 
-    dplyr::mutate(across(where(is.character), ~factor(.)), # change all character columns in  to factors
-                  abundance = ifelse(abundance > 0, yes = 1, no = 0)) %>% # convert data into binary
+    dplyr::mutate(across(where(is.character), ~factor(.)) #, # change all character columns in  to factors
+                  #abundance = ifelse(abundance > 0, yes = 1, no = 0)
+                  ) %>% # convert data into binary
     dplyr::select(species, abundance, everything())
   
   sf <- combined %>% # convert into sf
-    sf::st_as_sf(sf_column_name = "geometry") %>% 
-    sf::st_centroid()
+    sf::st_as_sf(sf_column_name = "geometry")
   
   return(sf)
   
 }
 
-# Convert bathymetry data (.TIFF) to sf
-gebcoConvert <- function() {
+# Convert bathymetry data (.TIFF) to sf (based on the grid provided)
+gebcoConvert <- function(grid, area) {
+  
   path <- "Data/GEBCO"
   list <- list.files(path)
   x <- apply(outer(list, ".tif", stringr::str_detect), 1, all) %>% as.numeric()
   file <- which(x == 1)
   
-  gebco <- terra::rast(file.path(path, list[file[i]])) %>%  # call raster file
-    terra::aggregate(., fact = 10) # aggregate by a factor of 10
+  grid_tibble <- grid %>% 
+    tibble::as_tibble()
+  grid_filled <- list()
   
-  # convert into sf
-  gebco_sf <- gebco %>% 
-    terra::as.polygons(trunc = FALSE, dissolve = FALSE, na.rm = FALSE) %>% 
-    sf::st_as_sf(crs = lonlat)
-  
-  # TODO: interpolate into grid cells
+  for(i in 1:length(file)) {
+    
+    gebco <- terra::rast(file.path(path, list[file[i]])) %>% 
+      terra::aggregate(., fact = 10)
+    
+    # convert to sf object
+    gebco_reclassified_sf <- gebco %>% 
+      terra::as.polygons(trunc = FALSE, dissolve = FALSE, na.rm = FALSE) %>% 
+      sf::st_as_sf(crs = lonlat) %>% 
+      sf::st_transform(crs = moll)
+    
+    colnames(gebco_reclassified_sf)[1] <- "depth" # change column name of the first column
+    
+    time <- system.time(grid_filled[[i]] <- gebco_reclassified_sf %>% 
+                          st_interpolate_aw(grid, extensive = FALSE) %>% 
+                          tibble::as_tibble() %>% 
+                          left_join(grid_tibble, ., by = "geometry"))
+    
+    print(time)
+    
+    print("Interpolated")
+  }
+    
+    # rename columns
+    for(i in 1:8) {
+      name = paste0("depth", i)
+      grid_filled[[i]] %<>% dplyr::rename(!!sym(name) := depth)
+    }
+    
+    joined <- purrr::reduce(grid_filled, left_join, by = c("cellID", "geometry")) # join all 8 files
+    
+    joined %<>% 
+      dplyr::mutate(meanDepth = rowMeans(joined[, 3:10], na.rm = TRUE))
+    
+    joined_sf <- joined %>% # converting into sf
+      sf::st_as_sf(sf_column_name = "geometry") %>% 
+      dplyr::select(cellID, meanDepth, geometry)
+    
+    saveRDS(joined_sf, paste0(path, "/gebco", area, ".rds"))
+    
+    return(joined_sf)
   
 }
