da6867770863307a53df5bbc362fe99008198e8e
Tin Buenafe <78692266+tinbuenafe@users.noreply.github.com>
2022-10-05 06:24:39 +0800

    Creating new df

--- 01a_DataLayers_Assembling.R
+++ 01a_DataLayers_Assembling.R
@@ -17,18 +17,17 @@ grid_YFT <- sf::st_join(grid, YFT_sf, join = st_contains_properly, left = TRUE) 
 ####################
 # Climate
 # 1. Temperature
+# If needed to reprocess, run all of these...
 # Convert all models to sf
-tos_model_list <- c("BCC-CSM2-MR", "CMCC-CM2-SR5", "CMCC-ESM2", "FGOALS-f3-L", "FGOALS-g3", "MIROC6", "MIROC-ES2L",
-                    "MRI-ESM2-0", "NorESM2-LM")
-# Create them only for historical for now
-createLayers(tos_model_list, "historical", "tos") # 1956-1981 averages
-# Create the ensemble
-createEnsemble("historical", "tos")
+# tos_model_list <- c("BCC-CSM2-MR", "CMCC-CM2-SR5", "CMCC-ESM2", "FGOALS-f3-L", "FGOALS-g3", "MIROC6", "MIROC-ES2L",
+#                     "MRI-ESM2-0", "NorESM2-LM")
+# # Create them only for historical for now
+# createLayers(tos_model_list, "historical", "tos") # 1956-1981 averages
+# # Create the ensemble
+# createEnsemble("historical", "tos")
 
-tos <- nc2sf(model = "ACCESS-ESM1-5",
-             expt = "ssp585",
-             var = "tos") %>% 
-  dplyr::rename(tos = mean) %>% 
+tos <- readRDS("Data/Climatology/ensemble/Ensemble-historical-tos.rds") %>% 
+  dplyr::rename(tos = EnsembleMean) %>% # using the mean of the models
   sf::st_interpolate_aw(grid, extensive = FALSE) %>% # interpolate with planning units
   dplyr::as_tibble() %>% 
   dplyr::left_join(grid, ., by = "geometry") %>% # left_join with the grid
@@ -38,16 +37,15 @@ tos <- nc2sf(model = "ACCESS-ESM1-5",
   dplyr::select(cellID, tos_transformed, geometry)
 
 # 2. Oxygen
-o2os_model_list <- c("CMCC-ESM2", "MIROC-ES2L", "MRI-ESM2-0", "NorESM2-LM")
-# Create them only for historical for now
-createLayers(o2os_model_list, "historical", "o2os") # 1956-1981 averages
-# Create the ensemble
-createEnsemble("historical", "o2os")
+# # If needed to reprocess, run all of these...
+# o2os_model_list <- c("CMCC-ESM2", "MIROC-ES2L", "MRI-ESM2-0", "NorESM2-LM")
+# # Create them only for historical for now
+# createLayers(o2os_model_list, "historical", "o2os") # 1956-1981 averages
+# # Create the ensemble
+# createEnsemble("historical", "o2os")
 
-o2os <- nc2sf(model = "ACCESS-ESM1-5",
-              expt = "ssp585",
-              var = "o2os") %>% 
-  dplyr::rename(o2os = mean) %>% 
+o2os <- readRDS("Data/Climatology/ensemble/Ensemble-historical-o2os.rds") %>% 
+  dplyr::rename(o2os = EnsembleMean) %>% # using the mean of the models 
   sf::st_interpolate_aw(grid, extensive = FALSE) %>% # interpolate with planning units
   dplyr::as_tibble() %>% 
   dplyr::left_join(grid, ., by = "geometry") %>% # left_join with the grid
@@ -57,16 +55,15 @@ o2os <- nc2sf(model = "ACCESS-ESM1-5",
   dplyr::select(cellID, o2os_transformed, geometry)
 
 # 3. pH
-phos_model_list <- c("CMCC-ESM2", "MIROC-ES2L", "MRI-ESM2-0", "NorESM2-LM")
-# Create them only for historical for now
-createLayers(phos_model_list, "historical", "phos") # 1956-1981 averages
-# Create the ensemble
-createEnsemble("historical", "phos")
+# # If needed to reprocess, run all of these...
+# phos_model_list <- c("CMCC-ESM2", "MIROC-ES2L", "MRI-ESM2-0", "NorESM2-LM")
+# # Create them only for historical for now
+# createLayers(phos_model_list, "historical", "phos") # 1956-1981 averages
+# # Create the ensemble
+# createEnsemble("historical", "phos")
 
-phos <- nc2sf(model = "CMCC-ESM2",
-              expt = "ssp585",
-              var = "phos") %>% 
-  dplyr::rename(phos = mean) %>% 
+phos <- readRDS("Data/Climatology/ensemble/Ensemble-historical-phos.rds") %>% 
+  dplyr::rename(phos = EnsembleMean) %>% # using the mean of the models
   sf::st_interpolate_aw(grid, extensive = FALSE) %>% # interpolate with planning units
   dplyr::as_tibble() %>% 
   dplyr::left_join(grid, ., by = "geometry") %>% # left_join with the grid
@@ -76,11 +73,22 @@ phos <- nc2sf(model = "CMCC-ESM2",
   dplyr::select(cellID, phos_transformed, geometry)
 
 # 4. chlorophyll-a
-chlos_model_list <- c("CMCC-ESM2", "MIROC-ES2L", "MRI-ESM2-0", "NorESM2-LM")
-# Create them only for historical for now
-createLayers(chlos_model_list, "historical", "chlos")
-# Create the ensemble
-createEnsemble("historical", "chlos")
+# # If needed to reprocess, run all of these...
+# chlos_model_list <- c("CMCC-ESM2", "MIROC-ES2L", "MRI-ESM2-0", "NorESM2-LM")
+# # Create them only for historical for now
+# createLayers(chlos_model_list, "historical", "chlos")
+# # Create the ensemble
+# createEnsemble("historical", "chlos")
+
+chlos <- readRDS("Data/Climatology/ensemble/Ensemble-historical-chlos.rds") %>% 
+  dplyr::rename(chlos = EnsembleMean) %>% # using the mean of the models
+  sf::st_interpolate_aw(grid, extensive = FALSE) %>% # interpolate with planning units
+  dplyr::as_tibble() %>% 
+  dplyr::left_join(grid, ., by = "geometry") %>% # left_join with the grid
+  sf::st_as_sf(crs = moll) %>% 
+  replaceNN(., grid, "chlos") %>%
+  dplyr::as_tibble() %>% 
+  dplyr::select(cellID, chlos_transformed, geometry)
 
 # Bathymetry
 #bathy <- gebcoConvert(grid, 2500) # bathymetry data is extrapolated depending on the grid area provided
@@ -100,9 +108,10 @@ dist2coast <- readRDS("Data/CoastDistance.rds") %>%
 # Joining all the data with the species data
 YFT_full <- dplyr::left_join(tos, o2os, by = c("cellID", "geometry")) %>% 
   dplyr::left_join(., phos, by = c("cellID", "geometry")) %>% 
+  dplyr::left_join(., chlos, by = c("cellID", "geometry")) %>% 
   dplyr::left_join(., bathy, by = c("cellID", "geometry")) %>% 
   dplyr::left_join(., dist2coast, by = c("cellID")) %>% 
   dplyr::left_join(grid_YFT, ., by = c("cellID", "geometry")) %>%  # Join with species data
-  dplyr::select(cellID, species, abundance, season, longitude, latitude, tos_transformed, o2os_transformed, phos_transformed, meanDepth, coastDistance, geometry) # arrange columns
+  dplyr::select(cellID, species, abundance, season, longitude, latitude, tos_transformed, o2os_transformed, phos_transformed, chlos_transformed, meanDepth, coastDistance, geometry) # arrange columns
 
 write_csv(YFT_full, file = "Output/YFT_full.csv") # save full data
--- Data/Climatology/CDO_depthResolved_modified.sh
+++ Data/Climatology/CDO_depthResolved_modified.sh
@@ -9,25 +9,27 @@ do
     case "${flag}" in
         m) model=${OPTARG};;
         g) gridname=${OPTARG};;
-        v) var=${OPTARG};; # tos, o2os, phos, ...
-        u) units=${OPTARG};; # depth units
-        n) grid=${OPTARG};; # gn or gr
+        l) lonname=${OPTARG};;
+        t) latname=${OPTARG};;
+        d) depth=${OPTARG};; # tos or thetao
+        u) units=${OPTARG};;
     esac
 done
 
 echo "User inputs:";
 echo "Model: $model";
 echo "Grid to be selected: $gridname";
-echo "Variable name: $var";
+echo "Longitude name: $lonname";
+echo "Latitude name: $latname";
+echo "Surface or depth-resolved: $depth";
 echo "Depth units: $units";
-echo "Grid type: $grid";
 
 # Define directories
 
 indir="raw/"
 outdir="crop/"
 mergedir="merged/"
-finaldir="final/"
+finaldir="final1/"
 
 # Define latitudinal boundaries (longitude min and max are already defined)
 #latmin=-79
@@ -36,47 +38,43 @@ finaldir="final/"
 #lonmax=50
 
 # defining arrays
-declare -a ExpArray=("ssp585", "historical")
+declare -a ExpArray=("ssp585")
 
 for e in ${ExpArray[@]}; do
     echo $e
-        curr_files=($(ls ${indir}*$var*$model*$e*))
+        curr_files=($(ls ${indir}*$depth*$model*$e*))
         num_files=${#curr_files[@]}
 
         for((i=0; i<=num_files-1; i++)); do
             curr_file=${curr_files[i]}
-            #cdo -sinfov $curr_file # Print details
+            cdo -sinfov $curr_file # Print details
 
             out_name1="tmpfile1.nc"
             cdo -selgrid,$gridname $curr_file $out_name1
 
-            #if [[ "$model" == "AWI-CM-1-1-MR" ]] # For models with unstructured grids (possibly those < 100km in res?)
-            #    then
-            #    cdo remapcon,global_1 $out_name1 temporary.nc
-            #    out_name1="temporary.nc"
-            #else
-            #    :
-            #fi
+            if [[ "$model" == "AWI-CM-1-1-MR" ]] # For models with unstructured grids (possibly those < 100km in res?)
+                then
+                cdo remapcon,global_1 $out_name1 temporary.nc
+                out_name1="temporary.nc"
+            else
+                :
+            fi
 
             out_name2="tmpfile2.nc"
-            cdo -remapbil,global_1 $out_name1 $out_name2 # Remapping to 1x1 global grid
+            cdo -remapbil,global_1 $out_name1 $out_name2 # Remapping to 1x1 global grid; should I do this? There was at least one model that didn't span the globe...
 
             #out_name3="tmpfile3.nc"
             #cdo -sellonlatbox,$lonmin,$lonmax,$latmin,$latmax $out_name2 $out_name3 # crop the data
 
             out_name4="tmpfile4.nc"
-            cdo -L selyear,2015/2100 -selname,$var $out_name2 $out_name4
+            cdo -L selyear,2015/2100 -selname,$depth -chname,$lonname,lon -chname,$latname,lat $out_name2 $out_name4
 
-            if [[ "grid" == "gn" ]]
-                then
-                annual_name=${curr_file/_gn_/_cropped_} # Change name
-            else # if grid = "gr"
-                annual_name=${curr_file/_gr_/_cropped_}
-            fi
-
+            # Change frequency from monthly to yearly
+            annual_name=${curr_file/_gn_/_cropped_} # Change name
+            #annual_name=${curr_file/_gr_/_cropped_}
             annual_name=${annual_name/Omon/Oyr}
             annual_name=${annual_name/$indir/$outdir} # Change directory
-            cdo -yearmean $out_name4 $annual_name # Change frequency from monthly to yearly
+            cdo -yearmean $out_name4 $annual_name
 
             # Clean up
             rm $out_name1 $out_name2 $out_name3 $out_name4
@@ -86,10 +84,10 @@ for e in ${ExpArray[@]}; do
     merged_name=${annual_name::${#annual_name}-16} # Remove dates
     merged_name=${merged_name/_cropped_/_cropped_merged.nc} # Change name
     merged_name=${merged_name/$outdir/$mergedir} # Change directory
-    cdo -O -mergetime $outdir*$var*$model*$e* $merged_name
-    echo $outdir*$var*$model*$e*
+    cdo -O -mergetime $outdir*$depth*$model*$e* $merged_name
+    echo $outdir*$depth*$model*$e*
 
-    if [[ "$var" == "tos" ]] || [[ "$var" == "o2os" ]] || [[ "$var" == "phos" ]]
+    if [[ "$depth" == "tos" ]] || [[ "$depth" == "o2os" ]] || [[ "$depth" == "phos" ]]
         then 
             out_name5="tmpfile5.nc"
             cdo setmisstonn $merged_name $out_name5 # set missing valueto nearest neighborhood value
