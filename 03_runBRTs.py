6b621e9e0fa0d17a38cb327f3f3639907724b509
Tin <tinbuenafe@gmail.com>
2022-10-18 06:46:47 +0800

    Update 03_runBRTs.py

--- 03_runBRTs.py
+++ 03_runBRTs.py
@@ -1,12 +1,17 @@
-from sklearn.datasets import make_regression
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import r2_score
+from os import chdir, getcwd
 import pandas as pd
 import numpy as numpy
 
-X, y = make_regression(random_state=0)
+wd = getcwd()
+chdir(wd)
 
-df = pd.read_csv ('/Users/tinbuenafe/GitHub/LarvaDistModels/Output/YFT_full.csv')
+df = pd.read_csv ('Output/YFT_full.csv')
 df_filtered = df[df['abundance'].notnull()] # Filter for rows with abundance values
 df_filtered = df_filtered.drop('geometry', axis=1)
 
@@ -21,8 +26,8 @@ def season(row):
     else:
         return 3
     
-df_filtered['season_mut'] = df_filtered.apply(season, axis=1)
-numpy.unique(df_filtered['season_mut'])
+df_filtered['season_mut'] = df_filtered.apply(season, axis=1) # Change seasons to numeric factors instead of strings
+numpy.unique(df_filtered['season_mut']) # Check if it does what we want it to do
 
 # Create column for presence/absence
 df_filtered = df_filtered.assign(abundance_mut = [1 if abundance > 0 else 0 for abundance in df_filtered['abundance']])
@@ -32,19 +37,41 @@ numpy.unique(df_filtered['abundance_mut']) # Seems right
 df_filtered.columns.values # Check column names
 X,y = df_filtered.drop(['cellID','species','abundance', 'abundance_mut', 'season'], axis=1),df_filtered['abundance_mut']
 
-# Split the dataset to training and testing values using 5-fold crossvalidation
-kf = KFold(n_splits=5,random_state=42,shuffle=True)
-kf.get_n_splits(X)
+auc_list = []
+accuracy_list = []
+f_list = []
+r2_list = []
 
-for train_index,val_index in kf.split(X):
-    X_train,X_val = X.iloc[train_index],X.iloc[val_index],
-    y_train,y_val = y.iloc[train_index],y.iloc[val_index]
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42) #Stratified is to ensure that each fold of dataset has the same proportion of observations with a given label
+for train_index, test_index in skf.split(X, y):
+    
+    # Split into training and testing data
+   X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+   y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+   
+   gbr = GradientBoostingRegressor(loss='squared_error', n_estimators = 1000) # 
+   gbr.fit(X_train, y_train.values.ravel())
+   y_pred = gbr.predict(X_test)
+   
+   # Calculate AUC per fold
+   auc = roc_auc_score(y_test, y_pred, average = "macro")
+   auc_list.append(auc)
+   
+   # Calculate accuracy score per fold
+   #accuracy = accuracy_score(y_test, y_pred)
+   #accuracy_list.append(accuracy)
+   
+   # Calculate F1 score per fold
+   #fscore = f1_score(y_test, y_pred, average = "macro")
+   #f_list.append(fscore)
+   
+   # Calculate R2 per fold
+   r2 = r2_score(y_test, y_pred)
+   r2_list.append(r2)
+   
+   
+mean_auc_gbr = numpy.mean(auc_list, axis = 0)
+mean_accuracy_gbr = numpy.mean(accuracy_list, axis = 0)
+mean_fscore_gbr = numpy.mean(f_list, axis = 0)
 
-gradient_booster = GradientBoostingRegressor(loss='squared_error',learning_rate=0.01)
-gradient_booster.get_params()
-
-gradient_booster.fit(X_train,y_train)
-gradient_booster.score(X_train,y_train) # Gets the R2
-
-# Get the R2 on the validation set
-gradient_booster.score(X_val,y_val)
+# Accuracy score
