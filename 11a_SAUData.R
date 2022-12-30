4f7b97fbef9c7621e5e70f738249ddc7c577f345
Tin <tinbuenafe@gmail.com>
2022-12-30 10:31:15 +0800

    sauries model building

---
+++ 11a_SAUData.R
@@ -0,0 +1,76 @@
+# DESCRIPTION: SAU Dataset
+
+# Load preliminaries
+source("00_Utils.R")
+
+sf <- combineFish(species = "sauries") %>% 
+  sf::st_transform(crs = moll) %>% 
+  sf::st_centroid() # transform into point data
+
+seasons <- c("jan-mar", "apr-jun", "jul-sept", "oct-dec")
+for(s in 1:length(seasons)) {
+  gridded <- assembleGrid(grid, sf %>% dplyr::filter(season == seasons[s]))
+  
+  assign(paste("grid", "SAU", seasons[s], sep = "_"), gridded)
+}
+
+# Load yellowfin tuna datasets
+SAU_ds1 <- read_csv("Output/CSV/SAU_historical_jan-mar.csv", show_col_types = FALSE)
+SAU_ds2 <- read_csv("Output/CSV/SAU_historical_apr-jun.csv", show_col_types = FALSE)
+SAU_ds3 <- read_csv("Output/CSV/SAU_historical_jul-sept.csv", show_col_types = FALSE)
+SAU_ds4 <- read_csv("Output/CSV/SAU_historical_oct-dec.csv", show_col_types = FALSE)
+
+# Build model with known data only
+SAU_build <- dplyr::bind_rows(SAU_ds1 %>% dplyr::filter(!is.na(abundance)),
+                               SAU_ds2 %>% dplyr::filter(!is.na(abundance)),
+                               SAU_ds3 %>% dplyr::filter(!is.na(abundance)),
+                               SAU_ds4 %>% dplyr::filter(!is.na(abundance))) %>% 
+  dplyr::mutate(across(where(is.character), ~factor(.))) %>% # convert all characters to factors
+  dplyr::mutate(abundance_presence = case_when(abundance > 0 ~ 1,
+                                               abundance == 0 ~ 0), 
+                row = row_number()) %>%  # mutate the abundance data into 1s and 0s
+  dplyr::select(-geometry) %>% 
+  dplyr::select(row, cellID, species, abundance, abundance_presence, ocean, longitude, latitude, season, everything()) %>% # arrange columns
+  as.data.frame() #gbm.step doesn't work if it's a tibble...
+
+# We divide the data into train (training and validation) and test
+nrow(SAU_build) * 0.9 # = 11890
+
+set.seed(4411) # rerun this
+train <- slice_sample(SAU_build, n = 11890, replace = FALSE) # 90% training set
+saveRDS(train, "Output/Train/SAU_train.csv")
+test <- SAU_build[!SAU_build$row %in% train$row, ] # 10% testing set
+saveRDS(test, "Output/Test/SAU_test.csv")
+
+# Data.frame for predictions
+# January-March predictions
+SAU_predict_season1 <- SAU_ds1 %>% dplyr::filter(is.na(abundance)) %>% 
+  dplyr::mutate(across(where(is.character), ~factor(.))) %>% # convert all characters to factors
+  dplyr::select(-geometry, -abundance, -species) %>% 
+  dplyr::select(cellID, ocean, longitude, latitude, season, everything()) %>% # arrange columns
+  dplyr::mutate(season = "jan-mar") %>% 
+  as.data.frame() #gbm.step doesn't work if it's a tibble...
+
+# April-June
+SAU_predict_season2 <- SAU_ds2 %>% dplyr::filter(is.na(abundance)) %>% 
+  dplyr::mutate(across(where(is.character), ~factor(.))) %>% # convert all characters to factors
+  dplyr::select(-geometry, -abundance, -species) %>% 
+  dplyr::select(cellID, ocean, longitude, latitude, season, everything()) %>% # arrange columns
+  dplyr::mutate(season = "apr-jun") %>% 
+  as.data.frame() #gbm.step doesn't work if it's a tibble...
+
+# July-September
+SAU_predict_season3 <- SAU_ds3 %>% dplyr::filter(is.na(abundance)) %>% 
+  dplyr::mutate(across(where(is.character), ~factor(.))) %>% # convert all characters to factors
+  dplyr::select(-geometry, -abundance, -species) %>% 
+  dplyr::select(cellID, ocean, longitude, latitude, season, everything()) %>% # arrange columns
+  dplyr::mutate(season = "jul-sept") %>% 
+  as.data.frame() #gbm.step doesn't work if it's a tibble...
+
+# October-December
+SAU_predict_season4 <- SAU_ds4 %>% dplyr::filter(is.na(abundance)) %>% 
+  dplyr::mutate(across(where(is.character), ~factor(.))) %>% # convert all characters to factors
+  dplyr::select(-geometry, -abundance, -species) %>% 
+  dplyr::select(cellID, ocean, longitude, latitude, season, everything()) %>% # arrange columns
+  dplyr::mutate(season = "oct-dec") %>% 
+  as.data.frame() #gbm.step doesn't work if it's a tibble...
---
+++ 11b_SAUFeatureSelect_Predictors.R
@@ -0,0 +1,86 @@
+###########################
+## Load preliminaries ##
+###########################
+# Load SAU Data
+source("11a_SAUData.R")
+
+#######################################################
+## Model 1: Restricted plots (Additional predictors) ##
+#######################################################
+
+SAU_model1 <- dismo::gbm.step(data = train, gbm.x = c(7:20),
+                               gbm.y = 5, family = "bernoulli", n.folds = 5)
+saveRDS(SAU_model1, "Output/Models/SAU_model1.rds") # save the model
+#SAU_model1 <- readRDS("Output/Models/SAU_model1.rds") # load the model
+
+summary(SAU_model1) # get the relative importance of each of the predictors
+
+# AUCs
+SAU_model1$self.statistics$discrimination # Training AUC Score
+SAU_model1$cv.statistics$discrimination.mean # Validation AUC Score
+
+##### Plotting ##### 
+train_tmp <- train %>% 
+  dplyr::mutate(model = SAU_model1$fitted)
+preds <- gbm::predict.gbm(SAU_model1, test, n.trees = SAU_model1$gbm.call$best.trees, type = "response") # predict to test
+
+dismo::calc.deviance(test[, "abundance_presence"], preds, family = "bernoulli")
+.roc(test$abundance_presence, preds) # Get testing AUC
+
+test_tmp <- test %>% 
+  dplyr::mutate(model = preds)
+
+#### January-March
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "jan-mar", # season
+                        SAU_predict_season1 %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)), # rest of the ocean cells
+                        SAU_model1, # BRT model
+                        `grid_SAU_jan-mar` %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)) # grid of species for specific season
+)
+
+ggseason1 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("January-March")
+
+#### April-June
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "apr-jun", # season
+                        SAU_predict_season2 %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)), # rest of the ocean cells
+                        SAU_model1, # BRT model
+                        `grid_SAU_apr-jun` %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)) # grid of species for specific season
+)
+
+ggseason2 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("April-June")
+
+#### July-September
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "jul-sept", # season
+                        SAU_predict_season3 %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)), # rest of the ocean cells
+                        SAU_model1, # BRT model
+                        `grid_SAU_jul-sept` %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)) # grid of species for specific season
+)
+
+ggseason3 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("July-September")
+
+#### October-December
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "oct-dec", # season
+                        SAU_predict_season4 %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)), # rest of the ocean cells
+                        SAU_model1, # BRT model
+                        `grid_SAU_oct-dec` %>% dplyr::filter(latitude >= min(SAU_build$latitude) & latitude <= max(SAU_build$latitude)) # grid of species for specific season
+)
+
+ggseason4 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("October-December")
+
+#### Arrange seasonal plots
+ggseasons <- (ggseason1 + ggseason2) / (ggseason3 + ggseason4) + 
+  plot_layout(guides = "collect") +
+  plot_annotation("Model 1: Additional Predictors (AUC: 0.92)")
+
+ggsave(plot = ggseasons, filename = "Figures/SAU/SAU_model1.png", width = 27, height = 15, dpi = 600)
