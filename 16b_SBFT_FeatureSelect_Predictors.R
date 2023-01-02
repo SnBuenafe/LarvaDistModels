2bf1edb901b0b4399b2c4e659d62ba83b87c86a7
Tin <tinbuenafe@gmail.com>
2023-01-02 15:34:56 +0800

    SBFT model runs

---
+++ 16b_SBFT_FeatureSelect_Predictors.R
@@ -0,0 +1,86 @@
+###########################
+## Load preliminaries ##
+###########################
+# Load SBFT Data
+source("16a_SBFT_Data.R")
+
+#######################################################
+## Model 1: Restricted plots (Additional predictors) ##
+#######################################################
+
+SBFT_model1 <- dismo::gbm.step(data = train, gbm.x = c(7:20),
+                              gbm.y = 5, family = "bernoulli", n.folds = 5)
+saveRDS(SBFT_model1, "Output/Models/SBFT_model1.rds") # save the model
+#SBFT_model1 <- readRDS("Output/Models/SBFT_model1.rds") # load the model
+
+summary(SBFT_model1) # get the relative importance of each of the predictors
+
+# AUCs
+SBFT_model1$self.statistics$discrimination # Training AUC Score
+SBFT_model1$cv.statistics$discrimination.mean # Validation AUC Score
+
+##### Plotting ##### 
+train_tmp <- train %>% 
+  dplyr::mutate(model = SBFT_model1$fitted)
+preds <- gbm::predict.gbm(SBFT_model1, test, n.trees = SBFT_model1$gbm.call$best.trees, type = "response") # predict to test
+
+dismo::calc.deviance(test[, "abundance_presence"], preds, family = "bernoulli")
+.roc(test$abundance_presence, preds) # Get testing AUC
+
+test_tmp <- test %>% 
+  dplyr::mutate(model = preds)
+
+#### January-March
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "jan-mar", # season
+                        SBFT_predict_season1 %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)), # rest of the ocean cells
+                        SBFT_model1, # BRT model
+                        `grid_SBFT_jan-mar` %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)) # grid of species for specific season
+)
+
+ggseason1 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("January-March")
+
+#### April-June
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "apr-jun", # season
+                        SBFT_predict_season2 %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)), # rest of the ocean cells
+                        SBFT_model1, # BRT model
+                        `grid_SBFT_apr-jun` %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)) # grid of species for specific season
+)
+
+ggseason2 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("April-June")
+
+#### July-September
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "jul-sept", # season
+                        SBFT_predict_season3 %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)), # rest of the ocean cells
+                        SBFT_model1, # BRT model
+                        `grid_SBFT_jul-sept` %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)) # grid of species for specific season
+)
+
+ggseason3 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("July-September")
+
+#### October-December
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "oct-dec", # season
+                        SBFT_predict_season4 %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)), # rest of the ocean cells
+                        SBFT_model1, # BRT model
+                        `grid_SBFT_oct-dec` %>% dplyr::filter(latitude >= min(SBFT_build$latitude) & latitude <= max(SBFT_build$latitude)) # grid of species for specific season
+)
+
+ggseason4 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("October-December")
+
+#### Arrange seasonal plots
+ggseasons <- (ggseason1 + ggseason2) / (ggseason3 + ggseason4) + 
+  plot_layout(guides = "collect") +
+  plot_annotation("Model 1: Additional Predictors (AUC: 0.98)")
+
+ggsave(plot = ggseasons, filename = "Figures/SBFT/SBFT_model1.png", width = 27, height = 15, dpi = 600)
