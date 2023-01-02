c0e90613525a6ee6ddf66ee3e2df6331b30bc04b
Tin <tinbuenafe@gmail.com>
2023-01-02 15:50:49 +0800

    SLT model building

---
+++ 17d_SLT_ModelBuilding.R
@@ -0,0 +1,97 @@
+# DESCRIPTION: Plotting potential models for SLT
+
+###########################
+## Load preliminaries ##
+###########################
+# Load SLT data
+source("17a_SLT_Data.R")
+
+###########################################################################
+## Model 2: Best test AUC ##
+###########################################################################
+# Load the model 2
+SLT_model2 <- readRDS("Output/Models/SLT_model2.rds")
+
+# What are the min and max latitudes?
+min(SLT_build$latitude)
+max(SLT_build$latitude)
+
+train_tmp <- train %>% 
+  dplyr::mutate(model = SLT_model2$fitted)
+preds <- gbm::predict.gbm(SLT_model2, test, n.trees = SLT_model2$gbm.call$best.trees, type = "response") # predict to test
+
+test_tmp <- test %>% 
+  dplyr::mutate(model = preds)
+
+#### January-March
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "jan-mar", # season
+                        SLT_predict_season1 %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude)), # rest of the ocean cells
+                        SLT_model2, # BRT model
+                        `grid_SLT_jan-mar` %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude)) # grid of species for specific season with restricted ranges
+)
+
+ggseason1 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("January-March")
+
+#### April-June
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "apr-jun", # season
+                        SLT_predict_season2 %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude)), # rest of the ocean cells
+                        SLT_model2, # BRT model
+                        `grid_SLT_apr-jun` %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude))# grid of species for specific season
+)
+
+ggseason2 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("April-June")
+
+#### July-September
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "jul-sept", # season
+                        SLT_predict_season3 %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude)), # rest of the ocean cells
+                        SLT_model2, # BRT model
+                        `grid_SLT_jul-sept` %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude)) # grid of species for specific season
+)
+
+ggseason3 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("July-September")
+
+#### October-December
+gg <- plotSeasonPredict(train_tmp, # training object with model column (fitted values)
+                        test_tmp, # testing object with model column (predictions)
+                        "oct-dec", # season
+                        SLT_predict_season4 %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude)), # rest of the ocean cells
+                        SLT_model2, # BRT model
+                        `grid_SLT_oct-dec` %>% dplyr::filter(latitude >= min(SLT_build$latitude) & latitude <= max(SLT_build$latitude))# grid of species for specific season
+)
+
+ggseason4 <- plotModel(gg[[1]]) + # Plot the model
+  ggtitle("October-December")
+
+ggseasons <- (ggseason1 + ggseason2) / (ggseason3 + ggseason4) + 
+  plot_layout(guides = "collect") +
+  plot_annotation("Model 2: Best test AUC (AUC: 0.97)")
+
+ggsave(plot = ggseasons, filename = "Figures/SLT/SLT_model2.png", width = 27, height = 15, dpi = 600)
+
+#### Plot relative importance of variables ####
+rel_imp <- summary(SLT_model2)
+
+ggrel <- ggplot(data = rel_imp, aes(x = reorder(var, rel.inf), y = rel.inf)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(expand = c(0,0)) +
+  coord_flip() +
+  theme_classic()
+
+ggsave(plot = ggrel, filename = "Figures/SLT/SLT_model2_RelImportance.png", width = 7, height = 5, dpi = 300)
+
+#### Plot test vs predictors ####
+pdf(file = "Figures/SLT/SLT_model2_PredictorsTrain.pdf", width = 10, height = 8)
+gbm.plot.fits(SLT_model2)
+dev.off()
+
+ggpredictors <- plotPredictors(test_tmp)
+ggsave(filename = "Figures/SLT/SLT_model2_PredictorsTest.pdf", plot = ggpredictors, width = 12, height = 8, dpi = 300)
