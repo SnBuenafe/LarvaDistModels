4161a193091019f2198a56b47dc113e2ed0c1d1f
Tin <tinbuenafe@gmail.com>
2023-04-30 16:19:41 +1000

    new models

---
+++ 18a_LIT_Data.R
@@ -0,0 +1,71 @@
+# DESCRIPTION: Assembling little tuna dataset
+
+# Load preliminaries
+source("00_Preliminaries.R")
+input_dir <- here::here("Output", "CSV")
+
+# Define functions
+# Function to restrict adult distribution predictor to just little tuna
+restrict_predictor <- function(x){
+  x %<>%
+    dplyr::select(c(1:21, 41:43, 51)) %>%  # restrict the predictors
+    rowwise() %>% 
+    dplyr::mutate(adult = mean(c(Euthynnus_affinis, Euthynnus_alletteratus, Euthynnus_lineatus), na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    dplyr::mutate(adult = ifelse(is.na(adult), yes = 0, no = adult)) # replace NAs of adult predictions to 0s
+}
+
+# Function to hatch areas where adults are unlikely to be found
+restrict_adult <- function(x, y) {
+  sf <- x %>% 
+    dplyr::mutate(adult_cat = ifelse(adult >= 0.01, yes = 1, no = 0)) %>% # used a 0.01 threshold
+    dplyr::select(-geometry) %>% 
+    dplyr::left_join(., y) %>% 
+    sf::st_as_sf(crs = cCRS)
+}
+
+# Create species sf object
+sf <- combineFish(species = "little-tuna") %>% 
+  fSpatPlan_Convert2PacificCentered(., cCRS = cCRS) %>% 
+  sf::st_centroid() # transform into point data
+
+seasons <- c("jan-mar", "apr-jun", "jul-sept", "oct-dec")
+for(s in 1:length(seasons)) {
+  gridded <- assembleGrid(grid, sf %>% dplyr::filter(season == seasons[s]))
+  
+  assign(paste("grid", "LIT", seasons[s], sep = "_"), gridded)
+}
+
+# Load little tuna datasets
+LIT_ds1 <- read_csv(here::here(input_dir, "LIT_historical_jan-mar.csv"), show_col_types = FALSE) %>% # January-March
+  restrict_predictor()
+
+LIT_ds2 <- read_csv(here::here(input_dir, "LIT_historical_apr-jun.csv"), show_col_types = FALSE) %>% # April-June
+  restrict_predictor()
+
+LIT_ds3 <- read_csv(here::here(input_dir, "LIT_historical_jul-sept.csv"), show_col_types = FALSE) %>% # July-September
+  restrict_predictor()
+
+LIT_ds4 <- read_csv(here::here(input_dir, "LIT_historical_oct-dec.csv"), show_col_types = FALSE) %>% # October-December
+  restrict_predictor()
+
+# Build model with known data only
+LIT_build <- dplyr::bind_rows(LIT_ds1 %>% dplyr::filter(!is.na(abundance)),
+                               LIT_ds2 %>% dplyr::filter(!is.na(abundance)),
+                               LIT_ds3 %>% dplyr::filter(!is.na(abundance)),
+                               LIT_ds4 %>% dplyr::filter(!is.na(abundance))) %>% 
+  organize_build()
+
+# We divide the data into train (training and validation) and test
+nrow(LIT_build) * 0.9 # = 11063.7
+
+set.seed(492480)
+train <- slice_sample(LIT_build, n = 11063, replace = FALSE) # 90% training set
+test <- LIT_build[!LIT_build$row %in% train$row, ] # 10% testing set
+
+# Prepare data frame for predictions
+LIT_predict_season1 <- organize_predict(LIT_ds1) # January-March
+LIT_predict_season2 <- organize_predict(LIT_ds2) # April-June
+LIT_predict_season3 <- organize_predict(LIT_ds3) # July-September
+LIT_predict_season4 <- organize_predict(LIT_ds4) # October-December
+
--- Utils/organize_predict.R
+++ Utils/organize_predict.R
@@ -4,3 +4,10 @@ organize_predict <- function(x) {
     dplyr::filter(is.na(abundance)) %>% 
     organize_build()
 }
+
+# TODO: Get rid of this once I get annual values from cdo
+organize_predict_tentative <- function(x) {
+  x %<>%
+    dplyr::filter(is.na(species)) %>% 
+    organize_build()
+}
