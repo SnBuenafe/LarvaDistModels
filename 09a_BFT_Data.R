3e7518a539ed587832d9f80189e115e792734bbb
Tin Buenafe <78692266+SnBuenafe@users.noreply.github.com>
2023-07-09 00:16:41 +1000

    redoing BFT models

--- 09a_BFT_Data.R
+++ 09a_BFT_Data.R
@@ -4,6 +4,7 @@
 source("00_SetupGrid.R")
 source("00_Preliminaries.R")
 species <- "BFT"
+figure_dir <- here::here(figure_dir, species)
 
 # Function to restrict adult distribution predictor to just bluefin tunas
 restrict_predictor <- function(x){
@@ -13,15 +14,6 @@ restrict_predictor <- function(x){
     dplyr::mutate(adult = mean(c(Thunnus_orientalis), na.rm = TRUE)) %>% # Just looking at Pacific tuna adult distributions
     ungroup() %>% 
     dplyr::mutate(adult = ifelse(is.na(adult), yes = 0, no = adult)) # replace NAs of adult predictions to 0s
-}
-
-# Function to hatch areas where adults are unlikely to be found
-restrict_adult <- function(x, y) {
-  sf <- x %>% 
-    dplyr::mutate(adult_cat = ifelse(adult >= 0.01, yes = 1, no = 0)) %>% 
-    dplyr::select(-geometry) %>% 
-    dplyr::left_join(., y) %>% 
-    sf::st_as_sf(crs = cCRS)
 }
 
 # Create species sf object
@@ -37,16 +29,16 @@ for(s in 1:length(seasons)) {
 }
 
 # Load striped marlin datasets
-BFT_ds1 <- read_csv(here::here(input_dir, "BFT_historical_jan-mar.csv"), show_col_types = FALSE) %>% # January-March
+BFT_ds1 <- read_csv(here::here(input_dir, paste(species, "jan-mar.csv", sep = "_")), show_col_types = FALSE) %>% # January-March
   restrict_predictor()
 
-BFT_ds2 <- read_csv(here::here(input_dir, "BFT_historical_apr-jun.csv"), show_col_types = FALSE)  %>% # April-June
+BFT_ds2 <- read_csv(here::here(input_dir, paste(species, "apr-jun.csv", sep = "_")), show_col_types = FALSE)  %>% # April-June
   restrict_predictor()
 
-BFT_ds3 <- read_csv(here::here(input_dir, "BFT_historical_jul-sept.csv"), show_col_types = FALSE)  %>% # July-September
+BFT_ds3 <- read_csv(here::here(input_dir, paste(species, "jul-sept.csv", sep = "_")), show_col_types = FALSE)  %>% # July-September
   restrict_predictor()
 
-BFT_ds4 <- read_csv(here::here(input_dir, "BFT_historical_oct-dec.csv"), show_col_types = FALSE)  %>% # October-December
+BFT_ds4 <- read_csv(here::here(input_dir, paste(species, "oct-dec.csv", sep = "_")), show_col_types = FALSE)  %>% # October-December
   restrict_predictor()
 
 # Build model with known data only
@@ -57,10 +49,10 @@ BFT_build <- dplyr::bind_rows(BFT_ds1 %>% dplyr::filter(!is.na(abundance)),
   organize_build()
 
 # We divide the data into train (training and validation) and test
-nrow(BFT_build) * 0.9 # = 11063.7
+nrow(BFT_build) * 0.8 # = 9834.4
 
 set.seed(3265)
-train <- slice_sample(BFT_build, n = 11063, replace = FALSE) # 90% training set
+train <- slice_sample(BFT_build, n = 9834, replace = FALSE) # 90% training set
 test <- BFT_build[!BFT_build$row %in% train$row, ] # 10% testing set
 
 # Prepare data frame for predictions
--- 09b_BFT_ModelBuilding.R
+++ 09b_BFT_ModelBuilding.R
@@ -3,189 +3,92 @@
 # Load preliminaries
 source("09a_BFT_Data.R")
 
-#### Model 1: With longitude and latitude ####
-# Grid search
+#### Create model ####
+
+# With longitude and latitude
+# 5-fold grid search
 CVGrid <- CVgridSearch(train, test, tc = c(1, 2), bf = c(0.5, 0.75), lr = seq(0.005, 0.01, 0.001), pred_in = c(7:23, 25), resp_in = 5)
 
 print(CVGrid %>% dplyr::arrange(desc(test_AUC)), n = 1) # BEST TEST AUC
 
 # Building most optimal model
-BFT_model1 <- dismo::gbm.step(data = train, gbm.x = c(7:23, 25),
+BFT_model <- dismo::gbm.step(data = train, gbm.x = c(7:23, 25),
                               gbm.y = 5, family = "bernoulli", n.folds = 5,
-                              tree.complexity = 2, bag.fraction = 0.75, learning.rate = 0.008
+                              tree.complexity = 2, bag.fraction = 0.75, learning.rate = 0.009
 )
-saveRDS(BFT_model1, here::here(model_dir, paste(species, "model1.rds", sep = "_")))
-# BFT_model1 <- readRDS(here::here(model_dir, paste(species, "model1.rds", sep = "_")))
+saveRDS(BFT_model, here::here(model_dir, paste(species, "model.rds", sep = "_")))
+# BFT_model <- readRDS(here::here(model_dir, paste(species, "model.rds", sep = "_")))
 
 # Show the relative importance of each of the predictors
-summary(BFT_model1) 
+summary(BFT_model) 
+
+# Number of trees
+BFT_model$n.trees
 
 # Printing AUCs
-BFT_model1$self.statistics$discrimination # Training AUC Score
-BFT_model1$cv.statistics$discrimination.mean # Validation AUC Score
+BFT_model$self.statistics$discrimination # Training AUC Score
+BFT_model$cv.statistics$discrimination.mean # Validation AUC Score
 
 # Predict to the testing dataset
-preds <- gbm::predict.gbm(BFT_model1, test, n.trees = BFT_model1$gbm.call$best.trees, type = "response")
+preds <- gbm::predict.gbm(BFT_model, test, n.trees = BFT_model$gbm.call$best.trees, type = "response")
 dismo::calc.deviance(test[, "abundance_presence"], preds, family = "bernoulli")
 get_testAUC(test$abundance_presence, preds) # Print testing AUC
 
-# Plot maps
+#### Plotting maps ####
 train_tmp <- train %>% 
-  dplyr::mutate(model = BFT_model1$fitted)
+  dplyr::mutate(model = BFT_model$fitted)
 test_tmp <- test %>% 
   dplyr::mutate(model = preds)
-limits = c(0, 0.95)
+limits = c(0, 1)
 
 # January-March
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jan-mar", # season
-                        BFT_predict_season1, # rest of the ocean cells
-                        BFT_model1, # BRT model
-                        `grid_BFT_jan-mar` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "jan-mar", # season
+                            BFT_predict_season1, # rest of the ocean cells
+                            BFT_model, # BRT model
+                            `grid_BFT_jan-mar` # grid of species for specific season
 )
 
-gg1 <- plotModel(gg, limits)
-hatch1 <- plotHatch(gg1, gg, BFT_ds1)
+gg <- plotModel(gg_obj, limits) # plot extrapolation on Jan-Mar
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "jan-mar", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
 
 # April-June
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "apr-jun", # season
-                        BFT_predict_season2, # rest of the ocean cells
-                        BFT_model1, # BRT model
-                        `grid_BFT_apr-jun` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "apr-jun", # season
+                            BFT_predict_season2, # rest of the ocean cells
+                            BFT_model, # BRT model
+                            `grid_BFT_apr-jun` # grid of species for specific season
 )
 
-gg2 <- plotModel(gg, limits)
-hatch2 <- plotHatch(gg2, gg, BFT_ds2)
+gg <- plotModel(gg_obj, limits) # plot extrapolation on Apr-Jun
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "apr-jun", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
 
 # July-September
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jul-sept", # season
-                        BFT_predict_season3, # rest of the ocean cells
-                        BFT_model1, # BRT model
-                        `grid_BFT_jul-sept` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "jul-sept", # season
+                            BFT_predict_season3, # rest of the ocean cells
+                            BFT_model, # BRT model
+                            `grid_BFT_jul-sept` # grid of species for specific season
 )
 
-gg3 <- plotModel(gg, limits)
-hatch3 <- plotHatch(gg3, gg, BFT_ds3)
+gg <- plotModel(gg_obj, limits) # plot extrapolation on Jul-Sept
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "jul-sept", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
 
 # October-December
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "oct-dec", # season
-                        BFT_predict_season4, # rest of the ocean cells
-                        BFT_model1, # BRT model
-                        `grid_BFT_oct-dec` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "oct-dec", # season
+                            BFT_predict_season4, # rest of the ocean cells
+                            BFT_model, # BRT model
+                            `grid_BFT_oct-dec` # grid of species for specific season
 )
 
-gg4 <- plotModel(gg, limits)
-hatch4 <- plotHatch(gg4, gg, BFT_ds4)
-
-ggsquished <- (gg1 + gg2) / (gg3 + gg4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = ggsquished, filename = here::here(figure_dir, paste(species, "model1", "base.png", sep = "_")), width = 27, height = 15, dpi = 600)
-
-gghatch <- (hatch1 + hatch2) / (hatch3 + hatch4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = gghatch, filename = here::here(figure_dir, paste(species, "model1", "hatched.png")), width = 27, height = 15, dpi = 600)
-
-#### Model 2: Without longitude and latitude ####
-
-# 5-fold grid search
-CVGrid <- CVgridSearch(train, test, tc = c(1, 2), bf = c(0.5, 0.75), lr = seq(0.005, 0.01, 0.001), pred_in = c(9:23, 25), resp_in = 5) # with longitude and latitude
-
-print(CVGrid %>% dplyr::arrange(desc(test_AUC)), n = 1) # BEST TEST AUC
-
-# Building most optimal model
-BFT_model2 <- dismo::gbm.step(data = train, gbm.x = c(9:23, 25),
-                              gbm.y = 5, family = "bernoulli", n.folds = 5,
-                              tree.complexity = 1, bag.fraction = 0.75, learning.rate = 0.009
-)
-saveRDS(BFT_model2, here::here(output_dir, paste(species, "model2.rds", sep = "_")))
-# BFT_model2 <- readRDS(here::here(output_dir, paste(species, "model2.rds", sep = "_")))
-
-# Show the relative importance of each of the predictors
-summary(BFT_model2)
-
-# Printing AUCs
-BFT_model2$self.statistics$discrimination # Training AUC Score
-BFT_model2$cv.statistics$discrimination.mean # Validation AUC Score
-
-# Predict to the testing dataset
-preds <- gbm::predict.gbm(BFT_model2, test, n.trees = BFT_model2$gbm.call$best.trees, type = "response")
-dismo::calc.deviance(test[, "abundance_presence"], preds, family = "bernoulli")
-get_testAUC(test$abundance_presence, preds) # Print testing AUC
-
-# Plot maps
-train_tmp <- train %>% 
-  dplyr::mutate(model = BFT_model2$fitted)
-test_tmp <- test %>% 
-  dplyr::mutate(model = preds)
-limits = c(0, 0.95)
-
-# January-March
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jan-mar", # season
-                        BFT_predict_season1, # rest of the ocean cells
-                        BFT_model2, # BRT model
-                        `grid_BFT_jan-mar` # grid of species for specific season
-)
-
-gg1 <- plotModel(gg, limits)
-hatch1 <- plotHatch(gg1, gg, BFT_ds1)
-
-# April-June
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "apr-jun", # season
-                        BFT_predict_season2, # rest of the ocean cells
-                        BFT_model2, # BRT model
-                        `grid_BFT_apr-jun` # grid of species for specific season
-)
-
-gg2 <- plotModel(gg, limits)
-hatch2 <- plotHatch(gg2, gg, BFT_ds2)
-
-# July-September
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jul-sept", # season
-                        BFT_predict_season3, # rest of the ocean cells
-                        BFT_model2, # BRT model
-                        `grid_BFT_jul-sept` # grid of species for specific season
-)
-
-gg3 <- plotModel(gg, limits)
-hatch3 <- plotHatch(gg3, gg, BFT_ds3)
-
-# October-December
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "oct-dec", # season
-                        BFT_predict_season4, # rest of the ocean cells
-                        BFT_model2, # BRT model
-                        `grid_BFT_oct-dec` # grid of species for specific season
-)
-
-gg4 <- plotModel(gg, limits)
-hatch4 <- plotHatch(gg4, gg, BFT_ds4)
-
-ggsquished <- (gg1 + gg2) / (gg3 + gg4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = ggsquished, filename = here::here(figure_dir, paste(species, "model2", "base.png", sep = "_")), width = 27, height = 15, dpi = 600)
-
-gghatch <- (hatch1 + hatch2) / (hatch3 + hatch4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = gghatch, filename = here::here(figure_dir, paste(species, "model2", "hatched.png", sep = "_")), width = 27, height = 15, dpi = 600)
+gg <- plotModel(gg_obj, limits) # plot extrapolation on Oct-Dec
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "oct-dec", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
