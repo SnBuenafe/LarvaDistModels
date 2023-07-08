1e79ee317c5f95e8b66e77b36f410fc43335e24f
Tin Buenafe <78692266+SnBuenafe@users.noreply.github.com>
2023-07-08 22:27:17 +1000

    Rerunning frigate tuna model

--- 07a_FRI_Data.R
+++ 07a_FRI_Data.R
@@ -4,6 +4,7 @@
 source("00_SetupGrid.R")
 source("00_Preliminaries.R")
 species <- "FRI"
+figure_dir <- here::here(figure_dir, species)
 
 # Function to restrict adult distribution predictor to just frigate tuna
 restrict_predictor <- function(x){
@@ -13,15 +14,6 @@ restrict_predictor <- function(x){
     dplyr::mutate(adult = mean(c(Auxis_rochei, Auxis_thazard), na.rm = TRUE)) %>% 
     ungroup() %>% 
     dplyr::mutate(adult = ifelse(is.na(adult), yes = 0, no = adult)) # replace NAs of adult predictions to 0s
-}
-
-# Function to hatch areas where adults are unlikely to be found
-restrict_adult <- function(x, y) {
-  sf <- x %>% 
-    dplyr::mutate(adult_cat = ifelse(adult >= 0.01, yes = 1, no = 0)) %>% 
-    dplyr::select(-geometry) %>% 
-    dplyr::left_join(., y) %>% 
-    sf::st_as_sf(crs = cCRS)
 }
 
 # Create species sf object
@@ -37,16 +29,16 @@ for(s in 1:length(seasons)) {
 }
 
 # Load frigate tuna datasets
-FRI_ds1 <- read_csv(here::here(input_dir, "FRI_historical_jan-mar.csv"), show_col_types = FALSE) %>% # January-March
+FRI_ds1 <- read_csv(here::here(input_dir, paste(species, "jan-mar.csv", sep = "_")), show_col_types = FALSE) %>% # January-March
   restrict_predictor()
 
-FRI_ds2 <- read_csv(here::here(input_dir, "FRI_historical_apr-jun.csv"), show_col_types = FALSE)  %>% # April-June
+FRI_ds2 <- read_csv(here::here(input_dir, paste(species, "apr-jun.csv", sep = "_")), show_col_types = FALSE)  %>% # April-June
   restrict_predictor()
 
-FRI_ds3 <- read_csv(here::here(input_dir, "FRI_historical_jul-sept.csv"), show_col_types = FALSE)  %>% # July-September
+FRI_ds3 <- read_csv(here::here(input_dir, paste(species, "jul-sept.csv", sep = "_")), show_col_types = FALSE)  %>% # July-September
   restrict_predictor()
 
-FRI_ds4 <- read_csv(here::here(input_dir, "FRI_historical_oct-dec.csv"), show_col_types = FALSE)  %>% # October-December
+FRI_ds4 <- read_csv(here::here(input_dir, paste(species, "oct-dec.csv", sep = "_")), show_col_types = FALSE)  %>% # October-December
   restrict_predictor()
 
 # Build model with known data only
@@ -57,10 +49,10 @@ FRI_build <- dplyr::bind_rows(FRI_ds1 %>% dplyr::filter(!is.na(abundance)),
   organize_build()
 
 # We divide the data into train (training and validation) and test
-nrow(FRI_build) * 0.9 # = 11063.7
+nrow(FRI_build) * 0.8 # = 9834.4
 
 set.seed(7500426)
-train <- slice_sample(FRI_build, n = 11063, replace = FALSE) # 90% training set
+train <- slice_sample(FRI_build, n = 9834, replace = FALSE) # 90% training set
 test <- FRI_build[!FRI_build$row %in% train$row, ] # 10% testing set
 
 # Prepare data frame for predictions
--- 07b_FRI_ModelBuilding.R
+++ 07b_FRI_ModelBuilding.R
@@ -3,188 +3,93 @@
 # Load preliminaries
 source("07a_FRI_Data.R") # Load FRI data
 
-#### Model 1: With longitude and latitude ####
+#### Create model ####
+
+# With longitude and latitude
 # 5-fold grid search
 CVGrid <- CVgridSearch(train, test, tc = c(1, 2), bf = c(0.5, 0.75), lr = seq(0.005, 0.01, 0.001), pred_in = c(7:23, 26), resp_in = 5)
 
 print(CVGrid %>% dplyr::arrange(desc(test_AUC)), n = 1) # BEST TEST AUC
 
 # Building most optimal model
-FRI_model1 <- dismo::gbm.step(data = train, gbm.x = c(7:23, 26),
+FRI_model <- dismo::gbm.step(data = train, gbm.x = c(7:23, 26),
                                gbm.y = 5, family = "bernoulli", n.folds = 5,
-                               tree.complexity = 2, bag.fraction = 0.5, learning.rate = 0.005
+                               tree.complexity = 2, bag.fraction = 0.5, learning.rate = 0.009
 )
-saveRDS(FRI_model1, here::here(model_dir, paste(species, "model1.rds", sep = "_")))
-# FRI_model1 <- readRDS(here::here(model_dir, paste(species, "model1.rds", sep = "_")))
+saveRDS(FRI_model, here::here(model_dir, paste(species, "model.rds", sep = "_")))
+# FRI_model <- readRDS(here::here(model_dir, paste(species, "model.rds", sep = "_")))
 
 # Show the relative importance of each of the predictors
-summary(FRI_model1)
+summary(FRI_model)
+
+# Number of trees
+FRI_model$n.trees
 
 # Printing AUCs
-FRI_model1$self.statistics$discrimination # Training AUC Score
-FRI_model1$cv.statistics$discrimination.mean # Validation AUC Score
+FRI_model$self.statistics$discrimination # Training AUC Score
+FRI_model$cv.statistics$discrimination.mean # Validation AUC Score
 
 # Predict to the testing dataset
-preds <- gbm::predict.gbm(FRI_model1, test, n.trees = FRI_model1$gbm.call$best.trees, type = "response")
+preds <- gbm::predict.gbm(FRI_model, test, n.trees = FRI_model$gbm.call$best.trees, type = "response")
 dismo::calc.deviance(test[, "abundance_presence"], preds, family = "bernoulli")
 get_testAUC(test$abundance_presence, preds) # Print testing AUC
 
-# Plot maps
+#### Plotting maps ####
+
 train_tmp <- train %>% 
-  dplyr::mutate(model = FRI_model1$fitted)
+  dplyr::mutate(model = FRI_model$fitted)
 test_tmp <- test %>% 
   dplyr::mutate(model = preds)
 limits = c(0, 1)
 
 # January-March
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jan-mar", # season
-                        FRI_predict_season1, # rest of the ocean cells
-                        FRI_model1, # BRT model
-                        `grid_FRI_jan-mar` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "jan-mar", # season
+                            FRI_predict_season1, # rest of the ocean cells
+                            FRI_model, # BRT model
+                            `grid_FRI_jan-mar` # grid of species for specific season
 )
 
-gg1 <- plotModel(gg, limits)
-hatch1 <- plotHatch(gg1, gg, FRI_ds1)
+gg <- plotModel(gg_obj, limits) # plot extrapolations for Jan-Mar
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "jan-mar", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
 
 # April-June
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "apr-jun", # season
-                        FRI_predict_season2, # rest of the ocean cells
-                        FRI_model1, # BRT model
-                        `grid_FRI_apr-jun` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "apr-jun", # season
+                            FRI_predict_season2, # rest of the ocean cells
+                            FRI_model, # BRT model
+                            `grid_FRI_apr-jun` # grid of species for specific season
 )
 
-gg2 <- plotModel(gg, limits)
-hatch2 <- plotHatch(gg2, gg, FRI_ds2)
+gg <- plotModel(gg_obj, limits) # plot extrapolations for Apr-Jun
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "apr-jun", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
 
 # July-September
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jul-sept", # season
-                        FRI_predict_season3, # rest of the ocean cells
-                        FRI_model1, # BRT model
-                        `grid_FRI_jul-sept` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "jul-sept", # season
+                            FRI_predict_season3, # rest of the ocean cells
+                            FRI_model, # BRT model
+                            `grid_FRI_jul-sept` # grid of species for specific season
 )
 
-gg3 <- plotModel(gg, limits)
-hatch3 <- plotHatch(gg3, gg, FRI_ds3)
+gg <- plotModel(gg_obj, limits) # plot extrapolations for Jul-Sept
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "jul-sept", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
 
 # October-December
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "oct-dec", # season
-                        FRI_predict_season4, # rest of the ocean cells
-                        FRI_model1, # BRT model
-                        `grid_FRI_oct-dec` # grid of species for specific season
+gg_obj <- create_speciesMap(train_tmp, # training object with model column (fitted values)
+                            test_tmp, # testing object with model column (predictions)
+                            "oct-dec", # season
+                            FRI_predict_season4, # rest of the ocean cells
+                            FRI_model, # BRT model
+                            `grid_FRI_oct-dec` # grid of species for specific season
 )
 
-gg4 <- plotModel(gg, limits)
-hatch4 <- plotHatch(gg4, gg, FRI_ds4)
-
-ggsquished <- (gg1 + gg2) / (gg3 + gg4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = ggsquished, filename = here::here(figure_dir, paste(species, "model1", "base.png", sep = "_")), width = 27, height = 15, dpi = 600)
-
-gghatch <- (hatch1 + hatch2) / (hatch3 + hatch4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = gghatch, filename = here::here(figure_dir, paste(species, "model1", "hatched.png", sep = "_")), width = 27, height = 15, dpi = 600)
-
-#### Model 2: Without longitude and latitude ####
-# 5-fold grid search
-CVGrid <- CVgridSearch(train, test, tc = c(1, 2), bf = c(0.5, 0.75), lr = seq(0.005, 0.01, 0.001), pred_in = c(9:23, 26), resp_in = 5)
-
-print(CVGrid %>% dplyr::arrange(desc(test_AUC)), n = 1) # BEST TEST AUC
-
-# Building most optimal model
-FRI_model2 <- dismo::gbm.step(data = train, gbm.x = c(9:23, 26),
-                               gbm.y = 5, family = "bernoulli", n.folds = 5,
-                               tree.complexity = 2, bag.fraction = 0.5, learning.rate = 0.007
-)
-saveRDS(FRI_model2, here::here(model_dir, "FRI_model2.rds"))
-# FRI_model2 <- readRDS(here::here(model_dir, "FRI_model2.rds"))
-
-# Show the relative importance of each of the predictors
-summary(FRI_model2)
-
-# Printing AUCs
-FRI_model1$self.statistics$discrimination # Training AUC Score
-FRI_model1$cv.statistics$discrimination.mean # Validation AUC Score
-
-# Predict to the testing dataset
-preds <- gbm::predict.gbm(FRI_model2, test, n.trees = FRI_model2$gbm.call$best.trees, type = "response")
-dismo::calc.deviance(test[, "abundance_presence"], preds, family = "bernoulli")
-get_testAUC(test$abundance_presence, preds) # Print testing AUC
-
-# Plot maps
-train_tmp <- train %>% 
-  dplyr::mutate(model = FRI_model2$fitted)
-test_tmp <- test %>% 
-  dplyr::mutate(model = preds)
-limits = c(0, 1)
-
-# January-March
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jan-mar", # season
-                        FRI_predict_season1, # rest of the ocean cells
-                        FRI_model2, # BRT model
-                        `grid_FRI_jan-mar` # grid of species for specific season
-)
-
-gg1 <- plotModel(gg, limits)
-hatch1 <- plotHatch(gg1, gg, FRI_ds1)
-
-# April-June
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "apr-jun", # season
-                        FRI_predict_season2, # rest of the ocean cells
-                        FRI_model2, # BRT model
-                        `grid_FRI_apr-jun` # grid of species for specific season
-)
-
-gg2 <- plotModel(gg, limits)
-hatch2 <- plotHatch(gg2, gg, FRI_ds2)
-
-# July-September
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "jul-sept", # season
-                        FRI_predict_season3, # rest of the ocean cells
-                        FRI_model2, # BRT model
-                        `grid_FRI_jul-sept` # grid of species for specific season
-)
-
-gg3 <- plotModel(gg, limits)
-hatch3 <- plotHatch(gg3, gg, FRI_ds3)
-
-# October-December
-gg <- create_speciesMap(train_tmp, # training object with model column (fitted values)
-                        test_tmp, # testing object with model column (predictions)
-                        "oct-dec", # season
-                        FRI_predict_season4, # rest of the ocean cells
-                        FRI_model2, # BRT model
-                        `grid_FRI_oct-dec` # grid of species for specific season
-)
-
-gg4 <- plotModel(gg, limits)
-hatch4 <- plotHatch(gg4, gg, FRI_ds4)
-
-ggsquished <- (gg1 + gg2) / (gg3 + gg4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = ggsquished, filename = here::here(figure_dir, paste(species, "model2", "base.png", sep = "_")), width = 27, height = 15, dpi = 600)
-
-gghatch <- (hatch1 + hatch2) / (hatch3 + hatch4) +
-  plot_annotation(tag_levels = "a", tag_prefix = "(", tag_suffix = ")") &
-  theme(plot.tag = element_text(size = 25))
-
-ggsave(plot = gghatch, filename = here::here(figure_dir, paste(species, "model2", "hatched.png", sep = "_")), width = 27, height = 15, dpi = 600)
+gg <- plotModel(gg_obj, limits) # plot extrapolations for Oct-Dec
+ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "oct-dec", "base.png", sep = "_")), 
+       width = 14, height = 5, dpi = 600)
