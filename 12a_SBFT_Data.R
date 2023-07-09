efa7fc4fcff2e49778af2e857435d822439b2913
Tin Buenafe <78692266+SnBuenafe@users.noreply.github.com>
2023-07-09 10:02:23 +1000

    Rerunning SBFT model

--- 12a_SBFT_Data.R
+++ 12a_SBFT_Data.R
@@ -4,6 +4,7 @@
 source("00_SetupGrid.R")
 source("00_Preliminaries.R")
 species <- "SBFT"
+figure_dir <- here::here(figure_dir, species)
 
 # Define functions
 # Function to restrict adult distribution predictor to just southern bluefin tuna
@@ -14,15 +15,6 @@ restrict_predictor <- function(x){
     dplyr::mutate(adult = mean(c(Thunnus_maccoyii), na.rm = TRUE)) %>% 
     ungroup() %>% 
     dplyr::mutate(adult = ifelse(is.na(adult), yes = 0, no = adult)) # replace NAs of adult predictions to 0s
-}
-
-# Function to hatch areas where adults are unlikely to be found
-restrict_adult <- function(x, y) {
-  sf <- x %>% 
-    dplyr::mutate(adult_cat = ifelse(adult >= 0.01, yes = 1, no = 0)) %>% # used a 0.01 threshold
-    dplyr::select(-geometry) %>% 
-    dplyr::left_join(., y) %>% 
-    sf::st_as_sf(crs = cCRS)
 }
 
 # Create species sf object
@@ -38,16 +30,16 @@ for(s in 1:length(seasons)) {
 }
 
 # Load southern bluefin tuna datasets
-SBFT_ds1 <- read_csv(here::here(input_dir, "SBFT_historical_jan-mar.csv"), show_col_types = FALSE) %>% # January-March
+SBFT_ds1 <- read_csv(here::here(input_dir, paste(species, "jan-mar.csv", sep = "_")), show_col_types = FALSE) %>% # January-March
   restrict_predictor()
 
-SBFT_ds2 <- read_csv(here::here(input_dir, "SBFT_historical_apr-jun.csv"), show_col_types = FALSE)  %>% # April-June
+SBFT_ds2 <- read_csv(here::here(input_dir, paste(species, "apr-jun.csv", sep = "_")), show_col_types = FALSE)  %>% # April-June
   restrict_predictor()
 
-SBFT_ds3 <- read_csv(here::here(input_dir, "SBFT_historical_jul-sept.csv"), show_col_types = FALSE)  %>% # July-September
+SBFT_ds3 <- read_csv(here::here(input_dir, paste(species, "jul-sept.csv", sep = "_")), show_col_types = FALSE)  %>% # July-September
   restrict_predictor()
 
-SBFT_ds4 <- read_csv(here::here(input_dir, "SBFT_historical_oct-dec.csv"), show_col_types = FALSE)  %>% # October-December
+SBFT_ds4 <- read_csv(here::here(input_dir, paste(species, "oct-dec.csv", sep = "_")), show_col_types = FALSE)  %>% # October-December
   restrict_predictor()
 
 # Build model with known data only
@@ -58,10 +50,10 @@ SBFT_build <- dplyr::bind_rows(SBFT_ds1 %>% dplyr::filter(!is.na(abundance)),
   organize_build()
 
 # We divide the data into train (training and validation) and test
-nrow(SBFT_build) * 0.9 # = 11063.7
+nrow(SBFT_build) * 0.8 # = 9834.4
 
 set.seed(8163519)
-train <- slice_sample(SBFT_build, n = 11063, replace = FALSE) # 90% training set
+train <- slice_sample(SBFT_build, n = 9834, replace = FALSE) # 90% training set
 test <- SBFT_build[!SBFT_build$row %in% train$row, ] # 10% testing set
 
 # Prepare data frame for predictions
