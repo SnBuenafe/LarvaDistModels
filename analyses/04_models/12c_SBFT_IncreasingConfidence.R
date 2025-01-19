cef857b860c5c570e504e146bab3cd67684d70f5
SnBuenafe <tinbuenafe@gmail.com>
2025-01-19 21:23:25 +1000

    Rerunning southern bluefin tuna models

--- analyses/04_models/12c_SBFT_IncreasingConfidence.R
+++ analyses/04_models/12c_SBFT_IncreasingConfidence.R
@@ -2,7 +2,7 @@
 
 # Load preliminaries
 source(file.path("analyses", "04_models", "12a_SBFT_Data.R")) # Load SBFT data
-model <- readRDS(here::here(model_dir, paste(species, "model.rds", sep = "_"))) # load model
+model <- readRDS(file.path(model_dir, paste(species, "model.rds", sep = "_"))) # load model
 
 # Making sure train and test data sets have the fitted predictions
 preds <- gbm::predict.gbm(model, test, n.trees = model$gbm.call$best.trees, type = "response")
@@ -30,18 +30,18 @@ gg_filt <- restrictThreshold(full_grid,
                              gg_obj,
                              5) %>% 
   restrictAQM("sbft")
-saveRDS(object = gg_filt, file = here::here(preds_dir, paste(species, "jan-mar.rds", sep = "_"))) # save predictions
-# gg_filt <- readRDS(here::here(preds_dir, paste(species, "jan-mar.rds", sep = "_")))
+saveRDS(object = gg_filt, file = file.path(preds_dir, paste(species, "jan-mar.rds", sep = "_"))) # save predictions
+# gg_filt <- readRDS(file.path(preds_dir, paste(species, "jan-mar.rds", sep = "_")))
 
 # Plot model removing 10x10 areas with lower confidence
 gg <- plotConfidence(gg_filt, full_grid, limits)
-ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "jan-mar", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600)
-write_rds(gg, file = here::here(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "1jan-mar", "highconf.rds", sep = "_")))
+ggsave(plot = gg, filename = file.path(figure_dir, paste(species, "jan-mar", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600)
+# write_rds(gg, file = file.path(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "1jan-mar", "highconf.rds", sep = "_")))
 
 # Plot the longitude and latitude bar plots
 bps <- plotLonLat(gg_filt, full_grid)
-ggsave(plot = bps$longitude, filename = here::here(figure_dir, paste(species, "longitude", "jan-mar.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
-ggsave(plot = bps$latitude, filename = here::here(figure_dir, paste(species, "latitude", "jan-mar.png", sep = "_")), width = 5, height = 1, dpi = 600)
+ggsave(plot = bps$longitude, filename = file.path(figure_dir, paste(species, "longitude", "jan-mar.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
+ggsave(plot = bps$latitude, filename = file.path(figure_dir, paste(species, "latitude", "jan-mar.png", sep = "_")), width = 5, height = 1, dpi = 600)
 
 #### April-June ####
 # Associate seasonal grids with the 10x10 grid
@@ -61,18 +61,18 @@ gg_filt <- restrictThreshold(full_grid,
                              gg_obj,
                              5) %>% 
   restrictAQM("sbft")
-saveRDS(object = gg_filt, file = here::here(preds_dir, paste(species, "apr-jun.rds", sep = "_"))) # save predictions
-# gg_filt <- readRDS(here::here(preds_dir, paste(species, "jan-mar.rds", sep = "_")))
+saveRDS(object = gg_filt, file = file.path(preds_dir, paste(species, "apr-jun.rds", sep = "_"))) # save predictions
+# gg_filt <- readRDS(file.path(preds_dir, paste(species, "jan-mar.rds", sep = "_")))
 
 # Plot model removing 10x10 areas with lower confidence
 gg <- plotConfidence(gg_filt, full_grid, limits)
-ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "apr-jun", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600) 
-write_rds(gg, file = here::here(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "2apr-jun", "highconf.rds", sep = "_")))
+ggsave(plot = gg, filename = file.path(figure_dir, paste(species, "apr-jun", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600) 
+# write_rds(gg, file = file.path(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "2apr-jun", "highconf.rds", sep = "_")))
 
 # Plot the longitude and latitude bar plots
 bps <- plotLonLat(gg_filt, full_grid)
-ggsave(plot = bps$longitude, filename = here::here(figure_dir, paste(species, "longitude", "apr-mar.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
-ggsave(plot = bps$latitude, filename = here::here(figure_dir, paste(species, "latitude", "apr-mar.png", sep = "_")), width = 5, height = 1, dpi = 600)
+ggsave(plot = bps$longitude, filename = file.path(figure_dir, paste(species, "longitude", "apr-mar.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
+ggsave(plot = bps$latitude, filename = file.path(figure_dir, paste(species, "latitude", "apr-mar.png", sep = "_")), width = 5, height = 1, dpi = 600)
 
 #### July-September ####
 # Associate seasonal grids with the 10x10 grid
@@ -92,18 +92,18 @@ gg_filt <- restrictThreshold(full_grid,
                              gg_obj,
                              5) %>% 
   restrictAQM("sbft")
-saveRDS(object = gg_filt, file = here::here(preds_dir, paste(species, "jul-sep.rds", sep = "_"))) # save predictions
-# gg_filt <- readRDS(here::here(preds_dir, paste(species, "jul-sep.rds", sep = "_")))
+saveRDS(object = gg_filt, file = file.path(preds_dir, paste(species, "jul-sep.rds", sep = "_"))) # save predictions
+# gg_filt <- readRDS(file.path(preds_dir, paste(species, "jul-sep.rds", sep = "_")))
 
 # Plot model removing 10x10 areas with lower confidence
 gg <- plotConfidence(gg_filt, full_grid, limits)
-ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "jul-sep", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600)
-write_rds(gg, file = here::here(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "3jul-sep", "highconf.rds", sep = "_")))
+ggsave(plot = gg, filename = file.path(figure_dir, paste(species, "jul-sep", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600)
+# write_rds(gg, file = file.path(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "3jul-sep", "highconf.rds", sep = "_")))
 
 # Plot the longitude and latitude bar plots
 bps <- plotLonLat(gg_filt, full_grid)
-ggsave(plot = bps$longitude, filename = here::here(figure_dir, paste(species, "longitude", "jul-sep.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
-ggsave(plot = bps$latitude, filename = here::here(figure_dir, paste(species, "latitude", "jul-sep.png", sep = "_")), width = 5, height = 1, dpi = 600)
+ggsave(plot = bps$longitude, filename = file.path(figure_dir, paste(species, "longitude", "jul-sep.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
+ggsave(plot = bps$latitude, filename = file.path(figure_dir, paste(species, "latitude", "jul-sep.png", sep = "_")), width = 5, height = 1, dpi = 600)
 
 #### October-December ####
 # Associate seasonal grids with the 10x10 grid
@@ -123,16 +123,16 @@ gg_filt <- restrictThreshold(full_grid,
                              gg_obj,
                              5) %>% 
   restrictAQM("sbft")
-saveRDS(object = gg_filt, file = here::here(preds_dir, paste(species, "oct-dec.rds", sep = "_"))) # save predictions
-# gg_filt <- readRDS(here::here(preds_dir, paste(species, "oct-dec.rds", sep = "_")))
+saveRDS(object = gg_filt, file = file.path(preds_dir, paste(species, "oct-dec.rds", sep = "_"))) # save predictions
+# gg_filt <- readRDS(file.path(preds_dir, paste(species, "oct-dec.rds", sep = "_")))
 
 # Plot model removing 10x10 areas with lower confidence
 gg <- plotConfidence(gg_filt, full_grid, limits)
-ggsave(plot = gg, filename = here::here(figure_dir, paste(species, "oct-dec", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600)
-write_rds(gg, file = here::here(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "4oct-dec", "highconf.rds", sep = "_")))
+ggsave(plot = gg, filename = file.path(figure_dir, paste(species, "oct-dec", "highconf.png", sep = "_")), width = 14, height = 5, dpi = 600)
+# write_rds(gg, file = file.path(str_remove(figure_dir, paste0("/",species)), "FigData", paste(species,  "4oct-dec", "highconf.rds", sep = "_")))
 
 # Plot the longitude and latitude bar plots
 bps <- plotLonLat(gg_filt, full_grid)
-ggsave(plot = bps$longitude, filename = here::here(figure_dir, paste(species, "longitude", "oct-dec.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
-ggsave(plot = bps$latitude, filename = here::here(figure_dir, paste(species, "latitude", "oct-dec.png", sep = "_")), width = 5, height = 1, dpi = 600)
+ggsave(plot = bps$longitude, filename = file.path(figure_dir, paste(species, "longitude", "oct-dec.png", sep = "_")), width = 7, height = 0.5, dpi = 600)
+ggsave(plot = bps$latitude, filename = file.path(figure_dir, paste(species, "latitude", "oct-dec.png", sep = "_")), width = 5, height = 1, dpi = 600)
 
