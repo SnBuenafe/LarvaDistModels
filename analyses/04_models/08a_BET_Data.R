0ebbaa6beddd92d7fdb250a4a7a0bf887546f3de
SnBuenafe <tinbuenafe@gmail.com>
2025-01-19 18:09:29 +1000

    Rerunning bigeye tuna models

--- analyses/04_models/08a_BET_Data.R
+++ analyses/04_models/08a_BET_Data.R
@@ -4,7 +4,7 @@
 source(file.path("analyses", "02_preliminaries", "00_SetupGrid.R"))
 source(file.path("analyses", "02_preliminaries", "00_Preliminaries.R"))
 species <- "BET"
-figure_dir <- here::here(figure_dir, species)
+figure_dir <- file.path(figure_dir, species)
 
 # Define functions
 # Function to restrict adult distribution predictor to just bigeye tunas
@@ -30,16 +30,16 @@ for(s in 1:length(seasons)) {
 }
 
 # Load bigeye tuna datasets
-BET_ds1 <- readRDS(here::here(input_dir, paste0(species, "_", seasons[1], ".rds"))) %>% 
+BET_ds1 <- readRDS(file.path(input_dir, paste0(species, "_", seasons[1], ".rds"))) %>% 
   restrict_predictor()
 
-BET_ds2 <- readRDS(here::here(input_dir, paste0(species, "_", seasons[2], ".rds"))) %>% 
+BET_ds2 <- readRDS(file.path(input_dir, paste0(species, "_", seasons[2], ".rds"))) %>% 
   restrict_predictor()
 
-BET_ds3 <- readRDS(here::here(input_dir, paste0(species, "_", seasons[3], ".rds"))) %>% 
+BET_ds3 <- readRDS(file.path(input_dir, paste0(species, "_", seasons[3], ".rds"))) %>% 
   restrict_predictor()
 
-BET_ds4 <- readRDS(here::here(input_dir, paste0(species, "_", seasons[4], ".rds"))) %>% 
+BET_ds4 <- readRDS(file.path(input_dir, paste0(species, "_", seasons[4], ".rds"))) %>% 
   restrict_predictor()
 
 # Build model with known data only
