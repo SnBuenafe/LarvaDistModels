03bfc966a858fd6039ba020231bd462ea4f6a170
SnBuenafe <tinbuenafe@gmail.com>
2025-01-22 13:55:39 +1000

    Rerunning calculations for spatial aggregation and seasonality indices

--- analyses/05_hotspot_analyses/22_DispersionIndex.R
+++ analyses/05_hotspot_analyses/22_DispersionIndex.R
@@ -1,6 +1,7 @@
-# DESCRIPTION: Calculating the index of dispersion (Coefficient of Variation - CV)
+# DESCRIPTION: Calculating the Spatial Aggregation Index and Seasonality Index
+
 source(file.path("analyses", "02_preliminaries", "00_Preliminaries.R"))
-# source(here::here("Utils", "fxnshemisphere.R"))
+# source(file.path("Utils", "fxnshemisphere.R"))
 
 pacman::p_load(purrr, magrittr, ggrepel, patchwork)
 
@@ -16,8 +17,11 @@ spec_dict %<>%
   dplyr::bind_cols(., groups = groups) %>% 
   dplyr::bind_cols(., lifehistory = LifeHistory)
 
-# Define the Spatial Aggregation Index (SAI, based on CV)
-# Logic: for each season for each species, the larger the sd 
+
+# Spatial Aggregation Index -----------------------------------------------
+
+# Define the Spatial Aggregation Index (SAI) based on coefficient of variation)
+# Logic: for each season for each species, the larger the sd the more spatially aggregated they are
 index <- function(x) {
   ind <- sd(x, na.rm = TRUE)/(mean(x, na.rm = TRUE))
 }
@@ -26,7 +30,7 @@ index <- function(x) {
 df_SAI <- list()
 for(i in 1:length(seasons)) {
   # Load file
-  tmp <- terra::rast(here::here(rast_dir, paste("ModelOutputs", paste0(seasons[i], ".tif"), sep = "_"))) %>% 
+  tmp <- terra::rast(file.path(rast_dir, paste("ModelOutputs", paste0(seasons[i], ".tif"), sep = "_"))) %>% 
     as.data.frame(xy = TRUE) %>% # get longitude and latitudes
     dplyr::as_tibble() %>% 
     dplyr::mutate(hemisphere = ifelse(y >= 0, "North", "South")) # separate them per hemisphere
@@ -54,11 +58,13 @@ full_SAI <- purrr::reduce(df_SAI, dplyr::left_join, by = c("species", "hemispher
   # dplyr::mutate(common = fct_reorder(common, desc(SAI))) %>% # We want to plot it with the common names
   # dplyr::bind_cols(., )
 
-# Calculate Seasonality Index (SI, aka Reviewer #2) for each Season
+
+# Seasonality Index -------------------------------------------------------
+# Seasonality Index = standard deviation (across seasons) of the mean of probabilities
 df_SI <- list()
 for(i in 1:length(seasons)) {
   # Load file
-  tmp <- terra::rast(here::here(rast_dir, paste("ModelOutputs", paste0(seasons[i], ".tif"), sep = "_"))) %>% 
+  tmp <- terra::rast(file.path(rast_dir, paste("ModelOutputs", paste0(seasons[i], ".tif"), sep = "_"))) %>% 
     as.data.frame(xy = TRUE) %>% # get longitude and latitudes
     dplyr::as_tibble() %>% 
     dplyr::mutate(hemisphere = ifelse(y >= 0, "North", "South")) # separate them per hemisphere
@@ -85,12 +91,14 @@ full_SI <- purrr::reduce(df_SI, dplyr::left_join, by = c("species", "hemisphere"
   dplyr::group_by(hemisphere) %>% 
   dplyr::left_join(., spec_dict, by = "code")
 
+# Calculate weighted mean latitudes ---------------------------------------
+
 # Calculate the weighted mean Lat for each species in each hemisphere
 # Calculate MnLat for each Season (for each Species and each Hemisphere)
 df_MnLat <- list()
 for(i in 1:length(seasons)) {
   # Load file
-  tmp <- terra::rast(here::here(rast_dir, paste("ModelOutputs", paste0(seasons[i], ".tif"), sep = "_"))) %>% 
+  tmp <- terra::rast(file.path(rast_dir, paste("ModelOutputs", paste0(seasons[i], ".tif"), sep = "_"))) %>% 
     as.data.frame(xy = TRUE) %>% # get longitude and latitudes
     dplyr::as_tibble() %>% 
     dplyr::mutate(hemisphere = ifelse(y >= 0, "North", "South")) # separate them per hemisphere
@@ -116,14 +124,15 @@ full_MnLat <- purrr::reduce(df_MnLat, dplyr::left_join, by = c("species", "hemis
   dplyr::group_by(hemisphere) %>% 
   dplyr::left_join(., spec_dict, by = "code")
 
+
+# Joining data frames -----------------------------------------------------
+
 # Join SAI and SI in one df
 full <- left_join(full_SAI, full_SI) #, by = c("common", "hemisphere"))
 full <- left_join(full, full_MnLat) #, by = c("common", "hemisphere"))
 
 # Remove single hemisphere species
-# SBFT in NH
-# BFT in SH
-# SLT in NH
+# remove: SBFT in NH, BFT in SH, and SLT in NH
 full <- full %>% mutate(SAI = replace(SAI, code == "SBFT" & hemisphere == "North", NA))
 full <- full %>% mutate(SI = replace(SI, code == "SBFT" & hemisphere == "North", NA))
 full <- full %>% mutate(SAI = replace(SAI, code == "BFT" & hemisphere == "South", NA))
@@ -131,6 +140,8 @@ full <- full %>% mutate(SI = replace(SI, code == "BFT" & hemisphere == "South", 
 full <- full %>% mutate(SAI = replace(SAI, code == "SLT" & hemisphere == "North", NA))
 full <- full %>% mutate(SI = replace(SI, code == "SLT" & hemisphere == "North", NA))
 
+
+# Exploring relationships between SAI and SI ------------------------------
 
 # What is the relationship between SI vs SAI
 m1 <- lm(SI ~ SAI, data = full)
@@ -148,35 +159,50 @@ full <- full %>%
 # plot SAI and SI vs Lat for groups
 theme_set(new = theme_bw(base_size = 25))
 
-# Plot Spatial Aggregation Index (full_SAI) vs Seasonality Index (full_SI)
+
+# SAI vs SI across hemispheres --------------------------------------------
+
+# Plot Spatial Aggregation Index (full_SAI) vs Seasonality Index (full_SI) across hemispheres
 ggplot(data = full, 
        aes(x = SAI, y = SI, label = code)) +
-  geom_point(size = 1.5, aes(shape = Hemisphere)) +
+  geom_smooth(method = "lm", alpha = 0.15, color = "grey64") +
+  geom_point(size = 5, aes(shape = Hemisphere, color = Hemisphere)) +
+  scale_shape_manual(values = c(16, 18)) +
+  scale_color_manual(aesthetics = "color",
+                     values = c("North" = "#0084C2", "South" = "#EAB47F")) +
   xlab("Spatial Aggregation Index") +
   ylab("Seasonality Index") +
-  geom_text_repel() +
-  scale_shape_manual(values = c(1, 16)) +
+  geom_label_repel(size = 5, aes(fill = Hemisphere), color = "white", segment.color = "black", show.legend = FALSE,
+                   box.padding = 0.5) +
+  scale_fill_manual(aesthetics = "fill",
+                    values = c("North" = "#0084C2", "South" = "#EAB47F")) +
   labs(shape = "Hemisphere") + 
-  geom_smooth(method = "lm") +
-  theme_bw()
+  theme_bw() +
+  theme(axis.text = element_text(size = 15, color = "black"),
+        axis.title = element_text(size = 15, color = "black"),
+        axis.ticks = element_line(color = "black"),
+        legend.text = element_text(size = 15),
+        legend.title = element_text(size = 15),
+        panel.border = element_rect(color = "black", linewidth = 1))
 
-ggsave(filename = here::here(figure_dir, "Dispersion.png"), dpi = 600, width = 15, height = 8, units = "in")
-
-# SAI vs SI (Groups)
-PtSize = 4
-ggplot(data = full, 
-       aes(x = SAI, y = SI, label = code)) +
-  geom_point(size = 1.5, aes(shape = groups)) +
-  xlab("Spatial Aggregation Index") +
-  ylab("Seasonality Index") +
-  geom_text_repel() +
-  geom_point(size = PtSize, aes(shape = groups, colour = groups)) +
-  geom_smooth(method = "lm") +
-  theme_bw()
+ggsave(filename = file.path(figure_dir, "SAI_SI_across_hemispheres.png"), dpi = 600, width = 15, height = 8, units = "in")
 
 
-# full2
-# average across hemispheres
+# SAI vs SI across taxonomic groups and life history strategies ---------------------------------------
+
+# # SAI vs SI across taxonomic groups
+# PtSize = 4
+# ggplot(data = full, 
+#        aes(x = SAI, y = SI, label = code)) +
+#   geom_point(size = 1.5, aes(shape = groups)) +
+#   xlab("Spatial Aggregation Index") +
+#   ylab("Seasonality Index") +
+#   geom_text_repel() +
+#   geom_point(size = PtSize, aes(shape = groups, colour = groups)) +
+#   geom_smooth(method = "lm") +
+#   theme_bw()
+
+# Take average across hemispheres
 full2 <- full %>% 
   group_by(code) %>% 
   summarise(Grps = first(groups), 
@@ -185,7 +211,7 @@ full2 <- full %>%
             MnSI = mean(SI, na.rm = TRUE), 
             MnLat = mean(abs(MnLat), na.rm = TRUE))
 
-# Groups
+# SAI vs SI across taxonomic groups
 p1 <- ggplot(data = full2,
              aes(x = MnSAI, y = MnSI, label = code)) +
   geom_smooth(method = "lm") +
@@ -194,7 +220,7 @@ p1 <- ggplot(data = full2,
   geom_text_repel() +
   geom_point(size = PtSize, aes(shape = Grps, colour = Grps))
 
-# Life history
+# SAI vs SI across life history strategies
 p2 <- ggplot(data = full2,
        aes(x = MnSAI, y = MnSI, label = code)) +
   geom_smooth(method = "lm") +
@@ -206,11 +232,11 @@ p2 <- ggplot(data = full2,
 p1 + p2 + plot_layout(ncol = 1, axes = "collect_x") & 
   theme(legend.position = "top")
 
-ggsave(filename = here::here(figure_dir, "LifeHistoryPlots.png"), dpi = 600, width = 12, height = 15, units = "in")
+# ggsave(filename = file.path(figure_dir, "LifeHistoryPlots.png"), dpi = 600, width = 12, height = 15, units = "in")
 
 # Investigate life history by functional group
-# Reorder Groups first
 
+# Reorder Groups first
 full2 <- full2 %>%
   mutate(Grps = fct_relevel(Grps, "Tuna", "Billfish", "Other"), 
          LH = fct_relevel(LH, "Slow", "Fast", "Unknown"))
@@ -219,61 +245,61 @@ p3 <- ggplot(data = full2,
              aes(x = Grps, y = MnSAI)) +
   geom_boxplot() +
   xlab("Groups") +
-  ylab("Spatial Aggregation Index")
+  ylab("Spatial Aggregation Index") +
+  theme(panel.border = element_rect(color = "black"),
+        axis.text = element_text(color = "black", size = 20),
+        axis.title = element_blank())
+
+ggsave(filename = file.path(figure_dir, "SAI_across_groups.png"), plot = p3, dpi = 600, width = 7, height = 5, units = "in")
 
 p4 <- ggplot(data = full2, 
             aes(x = Grps, y = MnSI)) +
   geom_boxplot() +
   xlab("Groups") +
-  ylab("Seasonality Index")
+  ylab("Seasonality Index") +
+  theme(panel.border = element_rect(color = "black"),
+        axis.text = element_text(color = "black", size = 20),
+        axis.title = element_blank())
 
+ggsave(filename = file.path(figure_dir, "SI_across_groups.png"), plot = p4, dpi = 600, width = 7, height = 5, units = "in")
 
 # Investigate life history by speed of growth (LH)
 p5 <- ggplot(data = full2, 
              aes(x = LH, y = MnSAI)) +
   geom_boxplot() +
-  xlab("Speed of life history") +
-  ylab("Spatial Aggregation Index")
+  xlab("Life history strategy") +
+  ylab("Spatial Aggregation Index") +
+  theme(panel.border = element_rect(color = "black"),
+        axis.text = element_text(color = "black", size = 20),
+        axis.title = element_blank())
+
+ggsave(filename = file.path(figure_dir, "SAI_across_life_history.png"), plot = p5, dpi = 600, width = 7, height = 5, units = "in")
 
 p6 <- ggplot(data = full2, 
              aes(x = LH, y = MnSI)) +
   geom_boxplot() +
-  xlab("Speed of life history") +
-  ylab("Seasonality Index")
+  xlab("Life history strategy") +
+  ylab("Seasonality Index") +
+  theme(panel.border = element_rect(color = "black"),
+        axis.text = element_text(color = "black", size = 20),
+        axis.title = element_blank())
 
+ggsave(filename = file.path(figure_dir, "SI_across_life_history.png"), plot = p6, dpi = 600, width = 7, height = 5, units = "in")
 
 # Put life history figures together
-Fig3 <- p3 + p4 + p5 + p6 + plot_layout(ncol = 2, byrow = FALSE, axes = "collect") +
-  plot_annotation(tag_levels = 'a')
+gg <- p3 + p4 + p5 + p6 + plot_layout(ncol = 2, byrow = FALSE, axes = "collect") +
+  plot_annotation(tag_levels = 'a',
+                  tag_prefix = '(',
+                  tag_suffix = ')') &
+  theme(plot.tag = element_text(size = 20, color = "black"))
 
-ggsave(filename = here::here(figure_dir, "Fig3_LH_Figure.png"), plot = Fig3, dpi = 600, width = 19, height = 15, units = "in")
-ggsave(filename = here::here(figure_dir, "Fig3_LH_Figure.pdf"), plot = Fig3, dpi = 600, width = 19, height = 15, units = "in")
+# ggsave(filename = file.path(figure_dir, "SAI_SI_across_groups.png"), plot = gg, dpi = 600, width = 15, height = 10, units = "in")
 
+
+# SAI vs SI across latitudes ----------------------------------------------
 
 # Investigate SAI and SI by Latitude
-p7 <- ggplot(data = full, 
-       aes(x = abs(MnLat), y = SAI, label = code)) + 
-  geom_smooth(method = "lm") +
-  xlab("Latitude (°S or °N)") +
-  ylab("Spatial Aggregation Index") +
-  geom_text_repel() +
-  geom_point(size = PtSize, aes(shape = Hemisphere, colour = Hemisphere))
-
-p8 <- ggplot(data = full, 
-             aes(x = abs(MnLat), y = SI, label = code)) +
-  geom_smooth(method = "lm") +
-  xlab("Latitude (°S or °N)") +
-  ylab("Seasonality Index") +
-  geom_text_repel() +
-  geom_point(size = PtSize, aes(shape = Hemisphere, colour = Hemisphere))
-
-Fig4 <- p7 + p8 + plot_layout(ncol = 1, guides = "collect", axes = "collect_x") + 
-  plot_annotation(tag_levels = 'a') & 
-  theme(legend.position = "top")
-
-ggsave(filename = here::here(figure_dir, "Fig4_IndicesPlots.png"), plot = Fig4, dpi = 600, width = 12, height = 17, units = "in")
-ggsave(filename = here::here(figure_dir, "Fig4_IndicesPlots.pdf"), plot = Fig4, dpi = 600, width = 12, height = 17, units = "in")
-
+ 
 # What is the relationship between SAI vs Lat and SI vs Lat?
 m1 <- lm(SAI ~ abs(MnLat), data = full)
 summary(m1)
