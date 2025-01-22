23bca122c0378d3a3a451299ef053876f85fa455
SnBuenafe <tinbuenafe@gmail.com>
2025-01-22 18:24:49 +1000

    Rerunning PCA analyses

--- analyses/06_hotspot_analyses/22c_PCA_Jul-Sept.R
+++ analyses/06_hotspot_analyses/22c_PCA_Jul-Sept.R
@@ -1,10 +1,10 @@
-# DESCRIPTION: Using PCA to determine hotspots for January-March
+# DESCRIPTION: Using PCA to determine hotspots for July-September
 
 # Define preliminaries
 source(file.path("analyses", "02_preliminaries", "00_SetupGrid.R"))
 source(file.path("analyses", "02_preliminaries", "00_Preliminaries.R"))
 pacman::p_load(purrr, Hmisc, RColorBrewer, patchwork)
-figure_dir <- here::here(figure_dir, "PCA")
+figure_dir <- file.path(figure_dir, "PCA")
 
 #### Prepare components for data frame ####
 spp_list <- spec_dict %>% 
@@ -15,35 +15,40 @@ spp_list <- spec_dict %>%
 
 df <- list() # empty list
 for(i in 1:length(spp_list)) {
-  obj <- readRDS(here::here(preds_dir, paste(toupper(spp_list[i]), "jan-mar.rds", sep = "_")))
+  obj <- readRDS(file.path(preds_dir, paste(toupper(spp_list[i]), "jul-sep.rds", sep = "_")))
   df[[i]] <- prepare_components(obj, spp_list[i])
 }
 
-df <- purrr::reduce(df, dplyr::left_join, by = c("cellID", "grid_100_category", "geometry"))
+# Function to replace NAs
+f <- function(x)ifelse(is.na(x), 0, x)
+
+df <- purrr::reduce(df, dplyr::left_join, by = c("cellID", "grid_100_category", "geometry")) 
 
 #### Run PCA ####
 PCA <- stats::princomp(df %>% 
-                         dplyr::select(-cellID, -grid_100_category, -geometry), cor = TRUE)
+                         dplyr::select(-cellID, -grid_100_category, -geometry) %>% 
+                         mutate(across(everything(), .fns = f)), cor = TRUE)
 
 summary(PCA)
 
 PC_scores <- PCA$scores[,1:2] %>% # first two axes
   tibble::as_tibble()
-write.csv(PC_scores, file = here::here(pc_dir, "hotspots_jan-mar_scores.csv"))
-# PC_scores <- read_csv(here::here(pc_dir, "hotspots_jan-mar_scores.csv"))
+write.csv(PC_scores, file = file.path(pc_dir, "hotspots_jul-sep_scores.csv"))
+# PC_scores <- read_csv(file.path(pc_dir, "hotspots_jul-sep_scores.csv"))
 
 loadings <- PCA$loadings %>% 
   as.data.frame.matrix()
-write.csv(loadings, file = here::here(pc_dir, "hotspots_jan-mar_loadings.csv"))
+write.csv(loadings, file = file.path(pc_dir, "hotspots_jul-sep_loadings.csv"))
 
 # Spearman correlation of PC scores with the individual maps
 cor_df <- df %>% 
+  mutate(across(!c("cellID", "grid_100_category", "geometry"), .fns = f)) %>% 
   dplyr::bind_cols(PC_scores, .)
 
 sp_cor <- cor(cor_df %>% 
                 dplyr::select(-cellID, -grid_100_category, -geometry),
               method = "spearman")
-write.csv(sp_cor, file = here::here(pc_dir, "hotspots_jan-mar_spearmancorr.csv"))
+write.csv(sp_cor, file = file.path(pc_dir, "hotspots_jul-sep_spearmancorr.csv"))
 
 #### Plotting PCA ####
 pc_obj <- df %>% 
@@ -53,14 +58,14 @@ pc_obj <- df %>%
 
 # Plot PC1
 pc1 <- plotPC1_limits(pc_obj, "Comp.1", "PC1 score")
-ggsave(plot = pc1, filename = here::here(figure_dir, "PCA1_jan-mar.png"), width = 14, height = 5, dpi = 600)
+ggsave(plot = pc1, filename = file.path(figure_dir, "PCA1_jul-sep.png"), width = 14, height = 5, dpi = 600)
 
 # Plot PC2
 pc2 <- plotPC2_limits(pc_obj, "Comp.2", "PC2 score  ")
-ggsave(plot = pc2, filename = here::here(figure_dir, "PCA2_jan-mar.png"), width = 14, height = 5, dpi = 600)
+ggsave(plot = pc2, filename = file.path(figure_dir, "PCA2_jul-sep.png"), width = 14, height = 5, dpi = 600)
 
 #### Plotting scree plots ####
 var_explained = PCA$sdev^2 / sum(PCA$sdev^2)
 scree_gg <- plotScree(var_explained)
 
-ggsave(plot = scree_gg, filename = here::here(figure_dir, "PCA_scree_jan-mar.png"), width = 7, height = 5, dpi = 600)
+ggsave(plot = scree_gg, filename = file.path(figure_dir, "PCA_scree_jul-sep.png"), width = 7, height = 5, dpi = 600)
